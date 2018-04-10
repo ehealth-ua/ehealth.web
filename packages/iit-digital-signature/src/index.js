7e60dcd7957fd9b344fba8569dfd7b4bcc3f2897
Myroslav Kiurchev <kiurchv@gmail.com>
2018-04-11 02:33:27 +0300

    feat(iit-digital-signature): add ability to fetch CAs list and certificates
    connects #19

--- packages/iit-digital-signature/src/index.js
+++ packages/iit-digital-signature/src/index.js
@@ -1,6 +1,13 @@
+require("whatwg-fetch");
+
 const euscptSource = require("../vendor/euscpt");
 const euscpmSource = require("../vendor/euscpm");
 const euscpSource = require("../vendor/euscp");
+
+const IIT_CA_CERTS_URL =
+  "https://iit.com.ua/download/productfiles/CACertificates.p7b";
+const IIT_CAS_URL = "https://iit.com.ua/download/productfiles/CAs.json";
+const DEVELOPMENT_PROXY = "http://localhost:5000/";
 
 var EU_MODULE_INITIALIZE_ON_LOAD = false;
 var EUSignCPModuleInitialized;
@@ -15,70 +22,126 @@ eval(euscpmSource);
 // Load EUSignCP core
 eval(euscpSource);
 
-const initialize = settings =>
+class DigitalSignature extends EUSignCP {
+  static async initialize(settings = {}) {
+    await initailizeModule();
+
+    const { proxy = DEVELOPMENT_PROXY } = settings;
+
+    const certificates = await fetchCertificates(proxy);
+    const authorities = await fetchAuthorities(proxy);
+
+    return new DigitalSignature({ certificates, authorities, proxy, settings });
+  }
+
+  constructor({ certificates, authorities, proxy, settings }) {
+    super();
+
+    this.authorities = authorities;
+    this.settings = settings;
+
+    this.setup();
+    this.SetXMLHTTPProxyService(proxy);
+    this.SaveCertificates(certificates);
+    this.setOcspAccessInfo();
+    this.setAuthoritySettings(0);
+  }
+
+  setup() {
+    const {
+      fileStoreSettings,
+      proxySettings,
+      tspSettings,
+      ocspSettings,
+      cmpSettings,
+      ldapSettings,
+      ocspAccessInfoModeSettings
+    } = this.settings;
+
+    this.SetFileStoreSettings(
+      Object.assign(this.CreateFileStoreSettings(), fileStoreSettings)
+    );
+
+    this.SetProxySettings(
+      Object.assign(this.CreateProxySettings(), proxySettings)
+    );
+
+    this.SetTSPSettings(Object.assign(this.CreateTSPSettings(), tspSettings));
+
+    this.SetOCSPSettings(
+      Object.assign(this.CreateOCSPSettings(), ocspSettings)
+    );
+
+    this.SetCMPSettings(Object.assign(this.CreateCMPSettings(), cmpSettings));
+
+    this.SetLDAPSettings(
+      Object.assign(this.CreateLDAPSettings(), ldapSettings)
+    );
+
+    if (ocspAccessInfoModeSettings) {
+      this.SetOCSPAccessInfoModeSettings(
+        Object.assign(
+          this.CreateOCSPAccessInfoModeSettings(),
+          ocspAccessInfoModeSettings
+        )
+      );
+    }
+  }
+
+  setOcspAccessInfo() {
+    this.authorities.forEach(
+      ({ issuerCNs, ocspAccessPointAddress, ocspAccessPointPort }) =>
+        issuerCNs.forEach(issuerCN => {
+          const settings = this.CreateOCSPAccessInfoSettings();
+          settings.SetAddress(ocspAccessPointAddress);
+          settings.SetPort(ocspAccessPointPort);
+          settings.SetIssuerCN(issuerCN);
+          this.SetOCSPAccessInfoSettings(settings);
+        })
+    );
+  }
+
+  setAuthoritySettings(index) {
+    if (index >= this.authorities.length) {
+      throw new Error("Wrong authority index");
+    }
+
+    const { cmpAddress, tspAddress } = this.authorities[index];
+
+    const cmpSettings = this.CreateCMPSettings();
+    cmpSettings.SetUseCMP(true);
+    cmpSettings.SetAddress(cmpAddress);
+    this.SetCMPSettings(cmpSettings);
+
+    const tspSettings = this.CreateTSPSettings();
+    tspSettings.SetGetStamps(true);
+    tspSettings.SetAddress(tspAddress);
+    this.SetTSPSettings(tspSettings);
+  }
+}
+
+const initailizeModule = () =>
   new Promise((resolve, reject) => {
-    EUSignCPModuleInitialized = success => {
-      if (!success) reject();
-
-      const instance = new EUSignCP();
-      setup(instance, settings);
-
-      resolve(instance);
-    };
-
+    EUSignCPModuleInitialized = success => (success ? resolve() : reject());
     EUSignCPModuleInitialize();
   });
 
-const setup = (instance, settings = {}) => {
-  const {
-    proxy = "http://localhost:5000/",
-    fileStoreSettings,
-    proxySettings,
-    tspSettings,
-    ocspSettings,
-    cmpSettings,
-    ldapSettings,
-    ocspAccessInfoModeSettings
-  } = settings;
-
-  instance.SetXMLHTTPProxyService(proxy);
-
-  instance.SetFileStoreSettings(
-    Object.assign(instance.CreateFileStoreSettings(), fileStoreSettings)
+const fetchCertificates = async proxy => {
+  const response = await fetch(
+    `${proxy}?raw=true&cache=true&address=${IIT_CA_CERTS_URL}`
   );
+  const buffer = await response.arrayBuffer();
+  return new Uint8Array(buffer);
+};
 
-  instance.SetProxySettings(
-    Object.assign(instance.CreateProxySettings(), proxySettings)
+const fetchAuthorities = async proxy => {
+  const authoritiesResponse = await fetch(
+    `${proxy}?raw=true&cache=true&address=${IIT_CAS_URL}`
   );
-
-  instance.SetTSPSettings(
-    Object.assign(instance.CreateTSPSettings(), tspSettings)
-  );
-
-  instance.SetOCSPSettings(
-    Object.assign(instance.CreateOCSPSettings(), ocspSettings)
-  );
-
-  instance.SetCMPSettings(
-    Object.assign(instance.CreateCMPSettings(), cmpSettings)
-  );
-
-  instance.SetLDAPSettings(
-    Object.assign(instance.CreateLDAPSettings(), ldapSettings)
-  );
-
-  if (ocspAccessInfoModeSettings) {
-    instance.SetOCSPAccessInfoModeSettings(
-      Object.assign(
-        instance.CreateOCSPAccessInfoModeSettings(),
-        ocspAccessInfoModeSettings
-      )
-    );
-  }
+  return authoritiesResponse.json();
 };
 
 module.exports = {
   __esModule: true,
-  default: EUSignCP,
-  initialize
+  default: DigitalSignature
 };
