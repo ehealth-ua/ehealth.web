97aa8b56fd413f2f75d6bdf9f298b969ee1c65b6
Myroslav Kiurchev <kiurchv@gmail.com>
2018-04-11 00:20:05 +0300

    feat(icons): add CheckRight icon

---
+++ packages/icons/src/CheckRightIcon.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CheckRightIcon = props => (
+  <svg {...props} viewBox="0 0 16 12">
+    <path
+      fill="currentColor"
+      d="M.91399 4.9915l2.12137-2.12097 3.74661 3.74583L12.9348.46475l2.0781 2.07766-6.15281 6.15163.24651.24646-2.1214 2.121z"
+    />
+  </svg>
+);
+
+export default CheckRightIcon;
---
+++ packages/icons/src/index.js
@@ -0,0 +1,1 @@
+export { default as CheckRightIcon } from "./CheckRightIcon";
