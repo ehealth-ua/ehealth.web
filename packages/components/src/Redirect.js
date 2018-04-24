1c4bb9a7d1ddaa20c4123bf3946fa565e92c4e26
Myroslav Kiurchev <kiurchv@gmail.com>
2018-04-24 17:11:29 +0300

    feat(components): add Redirect component

---
+++ packages/components/src/Redirect.js
@@ -0,0 +1,27 @@
+import { Component } from "react";
+import { withRouter } from "react-router";
+
+class Redirect extends Component {
+  componentDidMount() {
+    const { router, back, forward, to } = this.props;
+
+    if (!(back || forward || to))
+      throw new Error(
+        'Invariant Violation: Could not find any of "back", "forward" or "to" props in Redirect component.'
+      );
+
+    if (back) {
+      router.goBack();
+    } else if (forward) {
+      router.goForward();
+    } else {
+      router.push(to);
+    }
+  }
+
+  render() {
+    return null;
+  }
+}
+
+export default withRouter(Redirect);
--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -3,5 +3,6 @@ export { default as Button } from "./Button";
 export { default as Connect } from "./Connect";
 export { default as Field } from "./Field";
 export { default as Form } from "./Form";
+export { default as Redirect } from "./Redirect";
 export { default as Switch } from "./Switch";
 export { default as Validation, Validations } from "./Validation";
