3a4ff8fd1c057a0a83784f6ca157a9e5d6602556
ivchenkodima <ivchenkodima@gmail.com>
2018-04-26 12:24:57 +0300

    feat(icons): Add InfoIcon

--- packages/icons/src/index.js
+++ packages/icons/src/index.js
@@ -3,3 +3,4 @@ export { default as KeyIcon } from "./KeyIcon";
 export { default as ChevronLeftIcon } from "./ChevronLeftIcon";
 export { default as ChevronRightIcon } from "./ChevronRightIcon";
 export { default as CalendarIcon } from "./CalendarIcon";
+export { default as InfoIcon } from "./InfoIcon";
