2076bdbab2d4a90d89f6c09dcc0e3f541176f668
Myroslav Kiurchev <kiurchv@gmail.com>
2018-04-29 16:36:41 +0300

    fix(iit-digital-signature): transpile code to prevent unexpected errors when consuming by CRA apps

--- packages/iit-digital-signature/webpack.config.js
+++ packages/iit-digital-signature/webpack.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   mode: process.env.NODE_ENV,
   module: {
-    rules: [{ test: /vendor/, use: "raw-loader" }]
+    rules: [
+      { test: /\.js$/, include: /src/, use: "babel-loader" },
+      { test: /\.js$/, include: /vendor/, use: "raw-loader" }
+    ]
   },
   output: {
     libraryTarget: "commonjs2"
