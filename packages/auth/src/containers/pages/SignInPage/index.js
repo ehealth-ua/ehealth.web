030b8c3adf152f920a2fd2e31cf9b5c75bcb3694
ivchenkodima <ivchenkodima@gmail.com>
2018-05-03 21:10:41 +0300

    feat(auth): add verifying the cliend_id and redirect_uri in query in sign-in page, add DS button
    connects #5

--- packages/auth/src/containers/pages/SignInPage/index.js
+++ packages/auth/src/containers/pages/SignInPage/index.js
@@ -15,33 +15,71 @@ import SignInForm from "../../forms/SignInForm";
 
 import { onSubmit } from "./redux";
 
-const SignInPage = ({ onSubmit = () => {}, location }) => (
-  <Main id="sign-in-page">
-    <Header>
-      <H1>Вхід у систему eHealth</H1>
-    </Header>
-    <Article>
-      <NarrowContainer>
-        <SignInForm
-          onSubmit={onSubmit}
-          initialValues={{
-            email: location.query.email
-          }}
-        />
-        <ButtonsGroup>
-          <Button theme="link" to={`/update-password?${location.search}`}>
-            Змінити пароль
-          </Button>
-          <Button theme="link" to="/reset">
-            Забули пароль?
-          </Button>
-          <Button theme="link" to={`/update-factor/${location.search}`}>
-            Змінити додатковий фактор авторизації
-          </Button>
-        </ButtonsGroup>
-      </NarrowContainer>
-    </Article>
-  </Main>
-);
+class SignInPage extends React.Component {
+  renderNotFoundClientId() {
+    return (
+      <Main id="sign-in-page">
+        <Header>
+          <b>Помилка</b>
+        </Header>
+        <Article>
+          <p>Не вказан идентифікатор додатку для авторизації</p>
+        </Article>
+      </Main>
+    );
+  }
+  renderNotFoundRedirectUri() {
+    return (
+      <Main id="sign-in-page">
+        <Header>
+          <b>Помилка</b>
+        </Header>
+        <Article>
+          <p>Не вказано адресу зворотнього визову</p>
+        </Article>
+      </Main>
+    );
+  }
+  render() {
+    const {
+      onSubmit = () => {},
+      location: { search, query: { client_id, redirect_uri, email } }
+    } = this.props;
+    if (!client_id) return this.renderNotFoundClientId();
+    if (!redirect_uri) return this.renderNotFoundRedirectUri();
+
+    return (
+      <Main id="sign-in-page">
+        <Header>
+          <H1>Вхід у систему eHealth</H1>
+        </Header>
+        <Article>
+          <NarrowContainer>
+            <SignInForm
+              onSubmit={onSubmit}
+              initialValues={{
+                email
+              }}
+            />
+            <ButtonsGroup>
+              <Button color="blue" to={`sign-in/digital-sign?${search}`}>
+                увійти за допомогою ЕЦП
+              </Button>
+              <Button theme="link" to={`/update-password?${search}`}>
+                Змінити пароль
+              </Button>
+              <Button theme="link" to="/reset">
+                Забули пароль?
+              </Button>
+              <Button theme="link" to={`/update-factor/${search}`}>
+                Змінити додатковий фактор авторизації
+              </Button>
+            </ButtonsGroup>
+          </NarrowContainer>
+        </Article>
+      </Main>
+    );
+  }
+}
 
 export default compose(withRouter, connect(null, { onSubmit }))(SignInPage);
