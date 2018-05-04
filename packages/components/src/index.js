c7bd6e5e7b9523598fcd5607960ca11f50053102
Myroslav Kiurchev <kiurchv@gmail.com>
2018-05-04 15:31:11 +0300

    fix(components): add missing SUBMIT_ERROR export

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -2,7 +2,7 @@ export { default as Async } from "./Async";
 export { default as Button } from "./Button";
 export { default as Connect } from "./Connect";
 export { default as Field } from "./Field";
-export { default as Form } from "./Form";
+export { default as Form, SUBMIT_ERROR } from "./Form";
 export { default as Redirect } from "./Redirect";
 export { default as Switch } from "./Switch";
 export { default as Validation, Validations } from "./Validation";
