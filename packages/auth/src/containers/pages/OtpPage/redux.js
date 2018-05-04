e8d7c41cc1e3ed20c6a268aade69ecb0ccb614ac
ivchenkodima <ivchenkodima@gmail.com>
2018-05-04 14:48:49 +0300

    feat(auth): enable flow with default scope for cabinet client in otp page

--- packages/auth/src/containers/pages/OtpPage/redux.js
+++ packages/auth/src/containers/pages/OtpPage/redux.js
@@ -5,48 +5,46 @@ import { otpVerifyToken, otpResendOtp, authorize } from "../../../redux/auth";
 import { fetchUserData } from "../../../redux/user";
 import { login } from "../../../redux/session";
 
-export const onSubmit = ({ code }) => (dispatch, getState) =>
-  dispatch(otpVerifyToken(parseInt(code, 10))).then(action => {
-    if (action.error) {
-      const { type } = action.payload.response.error;
-      if (type) {
-        throw new SubmissionError({
-          code: {
-            [type]: true
-          }
-        });
-      }
-      return action;
-    }
-
-    dispatch(login(action.payload.value));
-    if (action.meta.next_step === "REQUEST_APPS") {
-      const state = getState();
-      const { routing: { locationBeforeTransitions: { query } } } = state;
-      return dispatch(
-        authorize({
-          clientId: query.client_id,
-          redirectUri: query.redirect_uri
-        })
-      ).then(({ payload, error }) => {
-        return window && (window.location = payload.headers.get("location"));
+export const onSubmit = ({ code }) => async (dispatch, getState) => {
+  const { payload: { value: token, details, response }, meta } = await dispatch(
+    otpVerifyToken(parseInt(code, 10))
+  );
+  if (response && response.error) {
+    const { type } = response.error;
+    if (type) {
+      throw new SubmissionError({
+        code: {
+          [type]: true
+        }
       });
     }
-
-    return dispatch(fetchUserData(action.payload.value)).then(action => {
-      if (action.error) {
-        return action;
-      }
-      const state = getState();
-      const location = getLocation(state);
-      return dispatch(
-        push({
-          ...location,
-          pathname: location.query.invite ? "/invite/accept" : "/accept"
-        })
-      );
+    return response.error;
+  }
+  dispatch(login(token));
+  if (meta.next_step === "REQUEST_APPS") {
+    return dispatch(
+      authorize({
+        clientId: details.client_id,
+        redirectUri: details.redirect_uri
+      })
+    ).then(({ payload, error }) => {
+      return window && (window.location = payload.headers.get("location"));
     });
+  }
+  return dispatch(fetchUserData(token)).then(action => {
+    if (action.error) {
+      return action;
+    }
+    const state = getState();
+    const location = getLocation(state);
+    return dispatch(
+      push({
+        ...location,
+        pathname: location.query.invite ? "/invite/accept" : "/accept"
+      })
+    );
   });
+};
 
 export const onResend = () => dispatch =>
   dispatch(otpResendOtp()).then(action => {
