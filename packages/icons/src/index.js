bba699326d30108a787fd913216792dd5cdc2f7b
Myroslav Kiurchev <kiurchv@gmail.com>
2018-05-08 16:44:34 +0300

    feat(icons): add EhealthLogoIcon

--- packages/icons/src/index.js
+++ packages/icons/src/index.js
@@ -3,4 +3,5 @@ export { default as KeyIcon } from "./KeyIcon";
 export { default as ChevronLeftIcon } from "./ChevronLeftIcon";
 export { default as ChevronRightIcon } from "./ChevronRightIcon";
 export { default as CalendarIcon } from "./CalendarIcon";
+export { default as EhealthLogoIcon } from "./EhealthLogoIcon";
 export { default as InfoIcon } from "./InfoIcon";
