bcdee70d99c7102005f1db0399b93ee8d203d9dd
ivchenkodima <ivchenkodima@gmail.com>
2018-05-11 14:57:32 +0300

    feat(patient-account): add Tabs component
    connects #2

---
+++ packages/patient-account/src/components/Tabs.js
@@ -0,0 +1,102 @@
+import React from "react";
+import styled from "react-emotion/macro";
+import { Slider, Slide } from "react-projector";
+import { ifProp } from "styled-tools";
+import { ResponsiveSlide } from "react-projector-responsive";
+
+const THEMES = {
+  large: "tabs_theme_large"
+};
+
+export default class Tabs extends React.Component {
+  state = {
+    activeTab: 0
+  };
+
+  render() {
+    const { theme, children } = this.props;
+    const { activeTab } = this.state;
+
+    return (
+      <div
+      // className={classnames("tabs", THEMES[theme])}
+      >
+        <TabControl
+          activeTab={activeTab}
+          onChange={this.changeTab}
+          tabs={children.map(({ title }) => title)}
+        />
+        <Content>
+          <Slider activeSlide={activeTab} onSlideChange={this.changeTab}>
+            {children.map(({ content }, index) => (
+              <Slide key={index} component={ResponsiveSlide}>
+                {content}
+              </Slide>
+            ))}
+          </Slider>
+        </Content>
+      </div>
+    );
+  }
+
+  changeTab = index => this.setState({ activeTab: index });
+}
+
+const TabControl = ({ tabs, activeTab, onChange = () => {} }) => (
+  <TabHeader>
+    <TabNav>
+      {tabs.map((title, index) => (
+        <TabItem
+          key={index}
+          active={activeTab === index}
+          onClick={() => onChange(index)}
+        >
+          {title}
+        </TabItem>
+      ))}
+    </TabNav>
+  </TabHeader>
+);
+
+const TabHeader = styled.div`
+  position: relative;
+
+  @media screen and (min-width: 768px) {
+    margin: 0 60px;
+  }
+`;
+
+const TabNav = styled.ul`
+  background: repeating-linear-gradient(
+    transparent,
+    transparent 54px,
+    #dad9d8 54px,
+    #dad9d8 55px
+  );
+  height: 55px;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  list-style: none;
+
+  @media screen and (min-width: 768px) {
+    justify-content: space-around;
+  }
+`;
+const TabItem = styled.li`
+  cursor: pointer;
+  margin: 0 5px 1px;
+  padding: 16px 0;
+  white-space: nowrap;
+  font-weight: 700;
+  box-shadow: ${ifProp("active", "inset 0 -5px 0 #4880ed")};
+
+  @media screen and (min-width: 768px) {
+    margin-left: 10px;
+    margin-right: 10px;
+  }
+`;
+const Content = styled.div`
+  position: relative;
+  margin-top: 60px;
+`;
