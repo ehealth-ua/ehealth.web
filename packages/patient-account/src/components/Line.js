777de4df7bbbdfa355d4c1453dbc98bcbc092d34
ivchenkodima <ivchenkodima@gmail.com>
2018-05-11 14:57:04 +0300

    feat(patient-account): add Line component

---
+++ packages/patient-account/src/components/Line.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "react-emotion/macro";
+import { ifProp, prop } from "styled-tools";
+
+const Line = styled.hr`
+  display: inline-block;
+  margin: 20px 0;
+  padding: 0;
+  width: ${ifProp("width", `${prop("width")}px`, "100%")};
+  height: 1px;
+  border: none;
+  background-color: #dedede;
+`;
+
+export default Line;
