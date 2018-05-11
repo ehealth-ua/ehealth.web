e41722d1bb6f75394646bceb2b5abdf9646dd9dd
ivchenkodima <ivchenkodima@gmail.com>
2018-05-11 14:56:20 +0300

    feat(patient-account): add Layout component

--- packages/patient-account/src/Routes.js
+++ packages/patient-account/src/Routes.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-
+import Layout from "./components/Layout";
 import HomePage from "./pages/HomePage";
 
 const Routes = () => (
-  <Switch>
-    <Route exact path="/" component={HomePage} />
-  </Switch>
+  <Layout>
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+    </Switch>
+  </Layout>
 );
 
 export default Routes;
---
+++ packages/patient-account/src/components/Layout.js
@@ -0,0 +1,40 @@
+import React from "react";
+import styled from "react-emotion/macro";
+import { EhealthLogoIcon } from "@ehealth/icons";
+
+const Layout = ({ children }) => (
+  <>
+    <Wrapper>
+      <Header>
+        <EhealthLogoIcon height="45" />
+        <div>Григорій Квітка-Основяненко</div>
+      </Header>
+      <section>{children}</section>
+    </Wrapper>
+    <Footer>©{new Date().getFullYear()} Всі права захищені</Footer>
+  </>
+);
+
+export default Layout;
+
+const Wrapper = styled.section`
+  max-width: 960px;
+  min-height: 100vh;
+  margin: 0 auto;
+  padding: 50px 10px 0;
+`;
+
+const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-bottom: 20px;
+`;
+
+const Footer = styled.footer`
+  flex: 0 0 40px;
+  line-height: 40px;
+  text-align: center;
+  padding: 40px 0;
+  border-top: 3px solid #a9da16;
+`;
