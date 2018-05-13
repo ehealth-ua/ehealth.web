cfdc92b32d9ddc0189a769f7c63246589662ded8
ivchenkodima <ivchenkodima@gmail.com>
2018-05-13 23:06:19 +0300

    feat(components): add CabinetTable component

---
+++ packages/components/src/CabinetTable.js
@@ -0,0 +1,61 @@
+import React from "react";
+import styled from "react-emotion/macro";
+import TableView from "./TableView";
+
+const CabinetTable = ({
+  data,
+  header,
+  renderRow,
+  rowKeyExtractor,
+  columnKeyExtractor
+}) => (
+  <TableView
+    data={data}
+    header={header}
+    renderRow={renderRow}
+    rowKeyExtractor={rowKeyExtractor}
+    columnKeyExtractor={columnKeyExtractor}
+    tableComponent={TableRoot}
+    headerComponent={TableHeader}
+    bodyComponent={TableBody}
+    rowComponent={TableRow}
+    headerCellComponent={TableHeaderCell}
+    cellComponent={TableCell}
+  />
+);
+
+export default CabinetTable;
+
+const TableRoot = styled.table`
+  border-collapse: collapse;
+  table-layout: fixed;
+  width: 100%;
+`;
+
+const TableHeader = styled.thead`
+  font-size: 10px;
+  user-select: none;
+`;
+
+const TableBody = styled.tbody`
+  background-color: #fff;
+`;
+
+const TableRow = styled.tr`
+  border-bottom: 1px solid #e7e7e9;
+`;
+
+const TableCell = styled.td`
+  font-size: 12px;
+  line-height: 18px;
+  padding: 17px 10px;
+  text-align: left;
+`;
+
+const TableHeaderCell = styled(TableCell.withComponent("th"))`
+  color: #2d2e39;
+  cursor: default;
+  font-weight: 700;
+  padding: 14px 10px;
+  text-transform: uppercase;
+`;
