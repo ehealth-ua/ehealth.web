c18b1ff067d34ac3bc8796b9bb1fa2d4c04fe576
Ihor Kolobanov <ikolobanov@MacBook-Pro-Ihor.local>
2018-05-16 12:22:57 +0300

    fix(components): rename Profile component

--- packages/patient-account/src/Routes.js
+++ packages/patient-account/src/Routes.js
@@ -2,13 +2,13 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Layout from "./components/Layout";
 import HomePage from "./pages/HomePage";
-import Profile from "./pages/Profile";
+import ProfilePage from "./pages/ProfilePage";
 
 const Routes = () => (
   <Layout>
     <Switch>
       <Route exact path="/" component={HomePage} />
-      <Route exact path="/profile" component={Profile} />
+      <Route exact path="/profile" component={ProfilePage} />
     </Switch>
   </Layout>
 );
