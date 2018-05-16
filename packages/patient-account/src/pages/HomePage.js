8ceb2459401e65a57037c429ff3340e35c782260
Myroslav Kiurchev <kiurchv@gmail.com>
2018-05-16 16:08:53 +0300

    chore(patient-account): add sample query

--- packages/patient-account/src/pages/HomePage.js
+++ packages/patient-account/src/pages/HomePage.js
@@ -1,5 +1,28 @@
 import React from "react";
+import { Query } from "react-apollo";
+import { gql } from "graphql.macro";
 
-const HomePage = () => "Hello world!";
+const HomePage = () => (
+  <Query
+    query={gql`
+      query {
+        declarations
+          @rest(path: "/cabinet/declarations", type: "DeclarationPayload") {
+          data
+        }
+      }
+    `}
+  >
+    {({ loading, error, data }) => (
+      <pre>
+        {loading
+          ? "Loading"
+          : error
+            ? JSON.stringify(error, null, 2)
+            : JSON.stringify(data, null, 2)}
+      </pre>
+    )}
+  </Query>
+);
 
 export default HomePage;
