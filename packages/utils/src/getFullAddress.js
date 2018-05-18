9811921ba35a9c78875e666297a822a01852bd58
Ihor Kolobanov <ikolobanov@MacBook-Pro-Ihor.local>
2018-05-18 12:23:02 +0300

    feat(utils): add settlement and street types in address

--- packages/utils/src/getFullAddress.js
+++ packages/utils/src/getFullAddress.js
@@ -1,22 +1,62 @@
+const streetTypeObj = {
+  LINE: "лінія",
+  PASS: "провулок",
+  ROAD: "дорога",
+  ALLEY: "алея",
+  BLOCK: "квартал",
+  TRACT: "урочище",
+  ASCENT: "узвіз",
+  AVENUE: "проспект",
+  MAIDAN: "майдан",
+  SQUARE: "площа",
+  STREET: "вулиця",
+  HIGHWAY: "шосе",
+  PASSAGE: "проїзд",
+  STATION: "станція",
+  ENTRANCE: "в’їзд",
+  FORESTRY: "лісництво",
+  BOULEVARD: "бульвар",
+  RIVER_SIDE: "набережна",
+  BLIND_STREET: "тупик",
+  HOUSING_AREA: "житловий масив",
+  MICRODISTRICT: "мікрорайон",
+  MILITARY_BASE: "військова частина",
+  SELECTION_BASE: "селекційна станція"
+};
+
+const settlementTypeObj = {
+  CITY: "місто",
+  VILLAGE: "село",
+  TOWNSHIP: "селище міського типу",
+  SETTLEMENT: "селище"
+};
+
 const getFullAddress = ({
   zip,
   country,
   area,
   region,
   settlement,
+  settlement_type,
   street,
+  street_type,
   building,
   apartment
 }) => {
+  const getStreet = [streetTypeObj[street_type], street].join(" ");
+  const getSettlement = [settlementTypeObj[settlement_type], settlement].join(
+    " "
+  );
+  const getAppartment = !apartment ? `кв.${apartment}` : null;
   const address = [
     zip,
     country,
-    `${area} Область`,
+    `${area} область`,
     region,
-    settlement,
-    street,
+    getSettlement,
+    getStreet,
     building,
-    `кв.${apartment}`
+    getAppartment
   ]
     .filter(Boolean)
     .join(", ");
