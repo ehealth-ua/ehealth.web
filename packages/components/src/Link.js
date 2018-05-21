71a9cf234f2eb2bfc494096b469c934f8adbab8d
Ihor Kolobanov <ikolobanov@MacBook-Pro-Ihor.local>
2018-05-21 09:50:30 +0300

    feat(components): Upgrade Link component

--- packages/components/src/Link.js
+++ packages/components/src/Link.js
@@ -1,17 +1,32 @@
 import React from "react";
 import styled from "react-emotion/macro";
 import { prop, ifProp, switchProp } from "styled-tools";
-import { Link as RouterLink } from "react-router-dom";
+import { NavLink as RouterLink } from "react-router-dom";
 
-const Link = ({ icon, to, children, ...rest }) => {
+const Link = ({ icon, to, rtl, children, ...rest }) => {
   const Component = LinkContainer.withComponent(to ? RouterLink : "a");
-
   return (
-    <Wrapper>
-      <Component {...rest}>{children}</Component>
-      {icon && <Icon>{icon}</Icon>}
+    <Wrapper rtl={rtl}>
+      <Component
+        {...rest}
+        to={to}
+        activeStyle={{
+          fontWeight: "bold"
+        }}
+      >
+        {children}
+      </Component>
+      {icon && (
+        <Icon rtl={rtl} size={rest.size}>
+          {icon}
+        </Icon>
+      )}
     </Wrapper>
   );
+};
+
+Link.defaultProps = {
+  color: "blue"
 };
 
 export default Link;
@@ -19,18 +34,25 @@ export default Link;
 const Wrapper = styled.div`
   display: flex;
   align-items: center;
+  flex-direction: ${props => props.rtl && "row-reverse"};
+  line-height: 1;
 `;
+
 const LinkContainer = styled.a`
   display: ${ifProp("block", "block", "inline-block")};
-  color: #2292f2;
+  color: ${switchProp("color", {
+    blue: "#2292f2",
+    black: "#333"
+  })};
   cursor: default;
-  font-weight: 700;
+  font-weight: ${props => props.bold && "bold"};
   font-size: ${switchProp("size", {
+    xs: "10px",
     small: "14px",
     medium: "18px",
     large: "22px"
   })};
-  letter-spacing: ${prop("theme.link.letterSpacing", 2.4)}px;
+  letter-spacing: ${props => props.letterSpacing && "2.4px"};
   text-align: center;
   text-transform: uppercase;
   text-decoration: none;
@@ -38,5 +60,7 @@ const LinkContainer = styled.a`
 `;
 
 const Icon = styled.span`
-  padding-left: 10px;
+  padding-right: ${props => props.rtl && "7px"};
+  padding-left: ${props => !props.rtl && "7px"};
+  vertical-align: middle;
 `;
