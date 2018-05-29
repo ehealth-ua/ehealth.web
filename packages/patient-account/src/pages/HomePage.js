2ed9573f6a40ca567171a843a77e0ebb876921c4
ivchenkodima <ivchenkodima@gmail.com>
2018-05-29 16:41:20 +0300

    feat(patient-account): add HomePage

--- packages/patient-account/src/pages/HomePage.js
+++ packages/patient-account/src/pages/HomePage.js
@@ -1,10 +1,27 @@
 import React from "react";
+<<<<<<< HEAD
 import { Title, Link } from "@ehealth/components";
+=======
+import styled from "react-emotion/macro";
+import { Query } from "react-apollo";
+import { gql } from "graphql.macro";
+import { Title, Link, CabinetTable, Switch } from "@ehealth/components";
+>>>>>>> feat(patient-account): add HomePage
 import { ArrowRight } from "@ehealth/icons";
 
 import Tabs from "../components/Tabs";
-import OrderedList from "../components/OrderedList";
-import Line from "../components/Line";
+
+import PatientDeclarations from "../blocks/PatientDeclarations";
+import NoDeclarationList from "../blocks/NoDeclarationList";
+
+const DeclarationQuery = gql`
+  query {
+    declarations
+      @rest(path: "/cabinet/declarations", type: "DeclarationsPayload") {
+      data
+    }
+  }
+`;
 
 const HomePage = () => (
   <>
@@ -14,6 +31,7 @@ const HomePage = () => (
         {
           title: "Моя декларація",
           content: (
+<<<<<<< HEAD
             <>
               <p>
                 Декларація відсутня. Для підписання декларації, виконайте
@@ -39,6 +57,18 @@ const HomePage = () => (
                 крок 1. оберіть лікаря
               </Link>
             </>
+=======
+            <Query query={DeclarationQuery}>
+              {({ loading, error, data = [] }) => {
+                if (!data.declarations) return <NoDeclarationList />;
+                return (
+                  <>
+                    <PatientDeclarations data={data.declarations.data} />
+                  </>
+                );
+              }}
+            </Query>
+>>>>>>> feat(patient-account): add HomePage
           )
         },
         { title: "Мої рецепти", content: "Сторінка в процесі розробки" }
@@ -46,5 +76,4 @@ const HomePage = () => (
     </Tabs>
   </>
 );
-
 export default HomePage;
