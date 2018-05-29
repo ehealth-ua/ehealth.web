1d498b5b0b4e0ad1ec336353816475103d5aaa52
ivchenkodima <ivchenkodima@gmail.com>
2018-05-29 16:40:12 +0300

    feat(patient-account): add NoDeclarationList block

---
+++ packages/patient-account/src/blocks/NoDeclarationList.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "@ehealth/components";
+
+import OrderedList from "../components/OrderedList";
+import Line from "../components/Line";
+import { ArrowRightIcon } from "@ehealth/icons";
+
+const NoDeclarationList = () => (
+  <>
+    <p>
+      Декларація відсутня. Для підписання декларації, виконайте наступні кроки
+    </p>
+    <OrderedList>
+      {[
+        "Оберіть лікаря",
+        "Створіть запит на декларацію",
+        "Очікуйте на підтвердження"
+      ]}
+    </OrderedList>
+    <Line />
+
+    <Link
+      to="/"
+      size="small"
+      upperCase
+      spaced
+      bold
+      icon={<ArrowRightIcon height="15px" fill="#2292f2" />}
+    >
+      крок 1. оберіть лікаря
+    </Link>
+  </>
+);
+
+export default NoDeclarationList;
