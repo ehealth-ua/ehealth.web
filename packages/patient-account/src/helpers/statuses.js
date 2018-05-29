e2603b28f16204e563debea3bce2cacca7ec71e4
ivchenkodima <ivchenkodima@gmail.com>
2018-05-29 16:41:58 +0300

    feat(patient-account): add declation statuses to helpers

---
+++ packages/patient-account/src/helpers/statuses.js
@@ -0,0 +1,8 @@
+const DECLARATION_STATUSES = {
+  terminated: "розірвана",
+  active: "активна",
+  closed: "припинена",
+  pending_verification: "Очікує підтвердження"
+};
+
+export default DECLARATION_STATUSES;
