2e12b936419d185cc8ae0f6e47f46e0feba3d024
ivchenkodima <ivchenkodima@gmail.com>
2018-05-31 15:45:15 +0300

    feat(patient-account): add DeclarationPage to routes

--- packages/patient-account/src/Routes.js
+++ packages/patient-account/src/Routes.js
@@ -4,6 +4,7 @@ import Layout from "./components/Layout";
 import HomePage from "./pages/HomePage";
 import ProfilePage from "./pages/ProfilePage";
 import ProfileEditPage from "./pages/ProfileEditPage";
+import DeclarationPage from "./pages/DeclarationPage";
 import SearchPage from "./pages/SearchPage";
 import EmployeePage from "./pages/EmployeePage";
 import DivisionPage from "./pages/DivisionPage";
@@ -14,6 +15,8 @@ const Routes = () => (
       <Route exact path="/" component={HomePage} />
       <Route exact path="/profile" component={ProfilePage} />
       <Route exact path="/profile/edit" component={ProfileEditPage} />
+      <Route exact path="/declarations/:id" component={DeclarationPage} />
+
       <Route exact path="/search" component={SearchPage} />
       <Route exact path="/search/employee/:id" component={EmployeePage} />
       <Route exact path="/search/division/:id" component={DivisionPage} />
