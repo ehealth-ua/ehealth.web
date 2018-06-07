a6fe2dee01c6adee9036aef04eb1d02fbd23bba8
ivchenkodima <ivchenkodima@gmail.com>
2018-06-07 12:58:42 +0300

    fix(patient-account): change btn history show/hide btn

--- packages/patient-account/src/components/PatientDeclarations.js
+++ packages/patient-account/src/components/PatientDeclarations.js
@@ -2,87 +2,44 @@ import React, { Fragment } from "react";
 import { Query } from "react-apollo";
 import { gql } from "graphql.macro";
 import styled from "react-emotion/macro";
-import format from "date-fns/format";
+import { Route } from "react-router-dom";
+// import { withRouter } from "react-router";
 
 import { CabinetTable, Link } from "@ehealth/components";
-import DECLARATION_STATUSES from "../helpers/statuses";
-
 import ActiveDeclaration from "../components/ActiveDeclaration";
+import DeclarationHistory from "../components/DeclarationHistory";
 
-class PatientDeclarations extends React.Component {
-  state = {
-    show: false
-  };
-  render() {
-    const { data } = this.props;
-    const { show } = this.state;
+const PatientDeclarations = ({ data, match }) => {
+  const [active] = data.filter(
+    ({ status }) => status === "active" || status === "pending_verification"
+  );
+  return (
+    <>
+      <ActiveDeclaration active={active} />
+      <ShowBlock>
+        <Link
+          to={`${match.url === "/" ? "/declarations" : "/"}`}
+          upperCase
+          bold
+          center
+        >
+          {`${
+            match.url === "/"
+              ? "Показати історію декларацій"
+              : "Cховати історію декларацій"
+          }`}
+        </Link>
+      </ShowBlock>
+      <Route
+        path="/declarations"
+        render={props => {
+          return <DeclarationHistory {...props} data={data} />;
+        }}
+      />
+    </>
+  );
+};
 
-    const [active] = data.filter(
-      ({ status }) => status === "active" || status === "pending_verification"
-    );
-    return (
-      <>
-        <ActiveDeclaration active={active} blur={show} />
-        <ShowBlock>
-          <Link
-            upperCase
-            bold
-            center
-            onClick={() =>
-              this.setState({
-                show: !this.state.show
-              })
-            }
-          >
-            Показати історію декларацій
-          </Link>
-        </ShowBlock>
-        {show && (
-          <CabinetTable
-            data={data}
-            renderRow={({
-              id: declaration_id,
-              start_date,
-              status,
-              employee: { party: { last_name, first_name, second_name } },
-              division: { name: division_name },
-              legal_entity: { name: legal_entity_name },
-              details
-            }) => ({
-              start_date: format(start_date, "DD.MM.YYYY"),
-              status: DECLARATION_STATUSES[status],
-              division_name,
-              employee: (
-                <Fragment>
-                  {last_name} {first_name} {second_name}
-                </Fragment>
-              ),
-              legal_entity_name,
-              action: (
-                <Link bold to={`/declarations/${declaration_id}`}>
-                  Показати деталі
-                </Link>
-              )
-            })}
-            header={{
-              start_date: "Дата ухвалення декларації",
-              status: "Статус",
-              division_name: "Назва відділення",
-              employee: (
-                <Fragment>
-                  ПІБ<br /> лікаря
-                </Fragment>
-              ),
-              doctor_contact: "Контакті дані лікаря",
-              legal_entity_name: "Медзаклад",
-              action: "Дія"
-            }}
-          />
-        )}
-      </>
-    );
-  }
-}
 export default PatientDeclarations;
 
 const ShowBlock = styled.div`
