c5ea887f8ed6e16bc0afa1d63e6637498ae4453e
ivchenkodima <ivchenkodima@gmail.com>
2018-06-08 15:54:44 +0300

    refactor(patient-account): styled link to Search

---
+++ packages/patient-account/src/components/PatientDeclarations.js
@@ -0,0 +1,60 @@
+import React, { Fragment } from "react";
+import { Query } from "react-apollo";
+import { gql } from "graphql.macro";
+import { ifProp, prop } from "styled-tools";
+import styled from "react-emotion/macro";
+import { Route } from "react-router-dom";
+import { ArrowRightIcon } from "@ehealth/icons";
+import { CabinetTable, Link } from "@ehealth/components";
+
+import Line from "../components/Line";
+import ActiveDeclaration from "../components/ActiveDeclaration";
+import DeclarationHistory from "../components/DeclarationHistory";
+
+const PatientDeclarations = ({ data, match }) => {
+  const active = data.find(
+    ({ status }) => status === "active" || status === "pending_verification"
+  );
+  return (
+    <>
+      <ActiveDeclaration active={active} />
+      <ShowBlock center>
+        <Link
+          to={match.url === "/" ? "/declarations" : "/"}
+          upperCase
+          bold
+          center
+        >
+          {match.url === "/"
+            ? "Показати історію декларацій"
+            : "Cховати історію декларацій"}
+        </Link>
+      </ShowBlock>
+      <Route
+        path="/declarations"
+        render={props => {
+          return <DeclarationHistory {...props} data={data} />;
+        }}
+      />
+      <ShowBlock>
+        <Link
+          to="/search"
+          size="small"
+          upperCase
+          spaced
+          bold
+          icon={<ArrowRightIcon height="15px" fill="#2292f2" />}
+        >
+          Пошук лікаря
+        </Link>
+      </ShowBlock>
+    </>
+  );
+};
+
+export default PatientDeclarations;
+
+const ShowBlock = styled.div`
+  text-align: ${ifProp("center", "center", "left")};
+  margin: 45px 0;
+`;
