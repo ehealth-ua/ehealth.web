1a409b8326bd74c0aef30df96da1d2ccd56d9aba
burlachenkovv <val.volkov@gmail.com>
2018-06-15 17:51:40 +0300

    feat(patient-account): add new queries and components displaying for Declaration Requests and Declaration History to the HomePage

--- packages/patient-account/src/pages/HomePage.js
+++ packages/patient-account/src/pages/HomePage.js
@@ -5,10 +5,18 @@ import { gql } from "graphql.macro";
 import { Title, Link, CabinetTable, Switch } from "@ehealth/components";
 import { ArrowRight } from "@ehealth/icons";
 
+import DeclarationItem from "../components/DeclarationItem";
 import Tabs from "../components/Tabs";
-import PatientDeclarations from "../components/PatientDeclarations";
+
 import DeclarationRequestsQuery from "../graphql/DeclarationRequestsQuery.graphql";
 import DeclarationsQuery from "../graphql/DeclarationsQuery.graphql";
+import NoDeclarationList from "../components/NoDeclarationList";
+import DeclarationHistory from "../components/DeclarationHistory";
+
+import { ArrowRightIcon } from "@ehealth/icons";
+import Line from "../components/Line";
+
+import { Route } from "react-router-dom";
 
 const HomePage = props => (
   <>
@@ -19,14 +27,83 @@ const HomePage = props => (
           title: "Моя декларація",
           content: (
             <Query query={DeclarationsQuery}>
-              {({ loading, error, data = [] }) => {
+              {({ loading, error, data }) => {
+                if (loading) return null;
+
+                const { match } = props;
+                const {
+                  declarations: { data: [declaration] },
+                  declarationRequests: { data: [declarationRequest] },
+                  declarationHistory: { data: [...declarationHistory] }
+                } = data;
+
                 return (
-                  !loading && (
-                    <PatientDeclarations
-                      data={data.declarations.data}
-                      {...props}
-                    />
-                  )
+                  <>
+                    {declaration || declarationRequest ? (
+                      <>
+                        {declaration && (
+                          <>
+                            <DeclarationItem
+                              declaration={declaration}
+                              onReject={true}
+                            />
+                            <Line />
+                          </>
+                        )}
+                        {declarationRequest && (
+                          <>
+                            <RequestTitle>Запит на декларацію</RequestTitle>
+                            <DeclarationItem
+                              declaration={declarationRequest}
+                              blur={true}
+                              type="declarationRequest"
+                            />
+                          </>
+                        )}
+
+                        {declarationHistory && (
+                          <>
+                            <ShowBlock>
+                              <Link
+                                to={match.url === "/" ? "/declarations" : "/"}
+                                upperCase
+                                bold
+                                center
+                              >
+                                {match.url === "/"
+                                  ? "Показати історію декларацій"
+                                  : "Cховати історію декларацій"}
+                              </Link>
+                            </ShowBlock>
+                            <Route
+                              path="/declarations"
+                              render={props => {
+                                return (
+                                  <DeclarationHistory
+                                    {...props}
+                                    data={declarationHistory}
+                                  />
+                                );
+                              }}
+                            />
+                          </>
+                        )}
+                        <Line />
+                        <Link
+                          to="/search"
+                          size="small"
+                          upperCase
+                          spaced
+                          bold
+                          icon={<ArrowRightIcon height="15px" fill="#2292f2" />}
+                        >
+                          Пошук лікаря
+                        </Link>
+                      </>
+                    ) : (
+                      <NoDeclarationList />
+                    )}
+                  </>
                 );
               }}
             </Query>
@@ -38,3 +115,15 @@ const HomePage = props => (
   </>
 );
 export default HomePage;
+
+const ShowBlock = styled.div`
+  text-align: center;
+  margin: 45px 0;
+`;
+
+const RequestTitle = styled.h3`
+  color: #000;
+  margin: 0 0 25px 0;
+  font-weight: 600;
+  font-size: 16px;
+`;
