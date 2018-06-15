79d4982637751a3c0f4eeacbed90fd161549bff4
burlachenkovv <val.volkov@gmail.com>
2018-06-15 17:45:10 +0300

    feat(patient-account): add new status for Declaration Request to the helpers

--- packages/patient-account/src/helpers/statuses.js
+++ packages/patient-account/src/helpers/statuses.js
@@ -2,7 +2,8 @@ const DECLARATION_STATUSES = {
   terminated: "розірвана",
   active: "активна",
   closed: "припинена",
-  pending_verification: "Очікує підтвердження"
+  pending_verification: "Очікує підтвердження",
+  APPROVED: "очікує підтвердження"
 };
 
 export default DECLARATION_STATUSES;
