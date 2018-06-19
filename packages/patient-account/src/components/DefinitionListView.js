5609355b70372041548d0b2e991174e380aa4ecd
Myroslav Kiurchev <kiurchv@gmail.com>
2018-06-20 02:05:57 +0300

    feat(patient-account): adopt theme values into DefinitionList, use appropriate html tags for DefinitionList items

--- packages/patient-account/src/components/DefinitionListView.js
+++ packages/patient-account/src/components/DefinitionListView.js
@@ -1,40 +1,54 @@
 import React from "react";
 import styled from "react-emotion/macro";
+import { css } from "react-emotion";
+import { prop } from "styled-tools";
 
 import { DefinitionList } from "@ehealth/components";
 
 const DefinitionListView = ({ title, labels, data }) => (
-  <DefinitionList
-    labels={labels}
-    data={data}
-    renderRow={(detail, labels) => (
-      <DefinitionWrapper>
-        <Term>{labels}</Term>
-        <Details>{detail}</Details>
-      </DefinitionWrapper>
-    )}
-  />
+  <List>
+    <DefinitionList
+      labels={labels}
+      data={data}
+      renderItem={({ label, value }) => (
+        <>
+          <Label>{label}</Label>
+          <Details>{value}</Details>
+        </>
+      )}
+    />
+  </List>
 );
 
-const DefinitionWrapper = styled.div`
+export default DefinitionListView;
+
+const List = styled.dl`
   display: flex;
   flex-wrap: wrap;
+  font-size: ${prop("theme.definitionList.fontSize")}px;
+`;
+
+const itemStyle = css`
   margin-bottom: 20px;
-  font-size: 14px;
-  line-height: 18px;
+
   &:last-of-type {
     margin-bottom: 0;
   }
 `;
 
-const Details = styled.div`
-  margin-bottom: 20px;
-`;
-
-const Term = styled(Details)`
-  flex-basis: 200px;
+const Label = styled.dt`
+  flex-basis: ${prop("theme.definitionList.labelWidth")}px;
   font-weight: 700;
   padding-right: 10px;
+  ${itemStyle};
 `;
 
-export default DefinitionListView;
+const Details = styled.dd`
+  flex-basis: ${prop("theme.definitionList.detailsWidth")}px;
+  margin-left: 0;
+  margin-right: calc(
+    100% - ${prop("theme.definitionList.labelWidth")}px -
+      ${prop("theme.definitionList.detailsWidth")}px
+  );
+  ${itemStyle};
+`;
