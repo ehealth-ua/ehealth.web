609e9382e273df089e4789badf46ecefc2122bca
Myroslav Kiurchev <kiurchv@gmail.com>
2018-06-20 01:22:58 +0300

    feat(components): add nil item filtering and ability to customize item keys to DefinitionList component

--- packages/components/src/DefinitionList.js
+++ packages/components/src/DefinitionList.js
@@ -1,9 +1,18 @@
 import React, { Fragment } from "react";
+import isNil from "lodash/isNil";
 
-const DefinitionList = ({ data, labels, renderRow }) =>
-  Object.entries(data).map(([key, text], index) => {
-    if (!text) return null;
-    return <Fragment key={index}>{renderRow(text, labels[key])}</Fragment>;
-  });
+const DefinitionList = ({
+  data,
+  labels,
+  renderItem,
+  keyExtractor = (name, index) => name
+}) =>
+  Object.entries(labels)
+    .filter(([name]) => !isNil(data[name]))
+    .map(([name, label], index) => (
+      <Fragment key={keyExtractor(name, index)}>
+        {renderItem({ label, value: data[name] })}
+      </Fragment>
+    ));
 
 export default DefinitionList;
