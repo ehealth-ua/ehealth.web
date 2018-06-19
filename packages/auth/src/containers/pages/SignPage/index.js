7fa59158189adf220d816b01558ae80c18475081
Myroslav Kiurchev <kiurchv@gmail.com>
2018-06-20 01:11:58 +0300

    feat(auth): add content signing logic to Sign page

--- packages/auth/src/containers/pages/SignPage/index.js
+++ packages/auth/src/containers/pages/SignPage/index.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
 import { Signer } from "@ehealth/react-iit-digital-signature";
 
-import { REACT_APP_ALLOWED_SIGN_ORIGINS } from "../../../env";
+import {
+  REACT_APP_ALLOWED_SIGN_ORIGINS,
+  REACT_APP_DIGITAL_SIGNATURE_ENABLED
+} from "../../../env";
 
 import {
   Main,
@@ -22,7 +25,37 @@ const SignPage = () => (
         <Signer.Child
           allowedOrigins={REACT_APP_ALLOWED_SIGN_ORIGINS.split(",")}
         >
-          {({ signData }) => <DigitalSignatureForm onSubmit={signData} />}
+          {({ data, onSignSuccess, onSignError }) =>
+            data && (
+              <DigitalSignatureForm
+                onSubmit={ds => {
+                  try {
+                    const content = JSON.stringify(data);
+
+                    const signedContent = REACT_APP_DIGITAL_SIGNATURE_ENABLED
+                      ? ds.SignDataInternal(true, content, true)
+                      : btoa(unescape(encodeURIComponent(content)));
+
+                    let meta;
+
+                    if (!REACT_APP_DIGITAL_SIGNATURE_ENABLED) {
+                      meta = {
+                        drfo: ds.privKeyOwnerInfo.subjDRFOCode,
+                        given_name: encodeURIComponent(
+                          ds.privKeySubject.GivenName
+                        ),
+                        surname: encodeURIComponent(ds.privKeySubject.SN)
+                      };
+                    }
+
+                    onSignSuccess({ signedContent, meta });
+                  } catch (error) {
+                    onSignError(error);
+                  }
+                }}
+              />
+            )
+          }
         </Signer.Child>
       </NarrowContainer>
     </Article>
