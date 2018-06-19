fb2584e7d40e285ae98ae2f0c046e36a4592443b
Myroslav Kiurchev <kiurchv@gmail.com>
2018-06-20 02:01:33 +0300

    feat(patient-account): add theme

--- packages/patient-account/src/App.js
+++ packages/patient-account/src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
+import { ThemeProvider } from "emotion-theming";
 import { ApolloProvider } from "react-apollo";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import "./globalStyles";
 import client from "./client";
+import theme from "./theme";
 import Routes from "./Routes";
 
 const App = () => (
   <ApolloProvider client={client}>
-    <Router>
-      <Routes />
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes />
+      </Router>
+    </ThemeProvider>
   </ApolloProvider>
 );
 
---
+++ packages/patient-account/src/theme.js
@@ -0,0 +1,15 @@
+const theme = {
+  input: {
+    labelFontSize: 14,
+    labelFontWeight: 700,
+    horizontalLabelWidth: 200,
+    horizontalContentWidth: 420
+  },
+  definitionList: {
+    fontSize: 14,
+    labelWidth: 200,
+    detailsWidth: 420
+  }
+};
+
+export default theme;
