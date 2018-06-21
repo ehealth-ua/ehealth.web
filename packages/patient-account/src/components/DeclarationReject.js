746cf76b856f857e08d253bf89f59d7102186570
burlachenkovv <val.volkov@gmail.com>
2018-06-21 17:56:21 +0300

    feat(patient-account): add DeclarationReject component

---
+++ packages/patient-account/src/components/DeclarationReject.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { ifProp } from "styled-tools";
+import { Mutation } from "react-apollo";
+import styled from "react-emotion/macro";
+
+import { Form, Field, Popup } from "@ehealth/components";
+
+import TerminateQuery from "../graphql/TerminateDeclarationQuery.graphql";
+
+const DeclarationReject = ({ onClose, id, onReject }) => (
+  <Mutation mutation={TerminateQuery}>
+    {terminateDeclaration => {
+      return (
+        <Popup onClose={onClose}>
+          <H2>Розірвання декларації</H2>
+          <P red={true}>
+            Ви зибраєтесь розірвати декларацію,<br /> цю дію не можна буде
+            відмінити.
+          </P>
+          <P>Ви впевнені, що хочете розірвати декларацію?</P>
+
+          <RejectForm
+            onSubmit={async ({ reason }) => {
+              try {
+                const res = await terminateDeclaration({
+                  variables: { id, input: { reason } }
+                });
+                onClose();
+                onReject();
+              } catch (error) {
+                console.log(error);
+              }
+            }}
+          >
+            <Field.MultilineText
+              name="reason"
+              placeholder="Причина розірвання (опціонально)"
+            />
+
+            <Form.Submit size="small">Розірвати декларацію</Form.Submit>
+          </RejectForm>
+        </Popup>
+      );
+    }}
+  </Mutation>
+);
+
+export default DeclarationReject;
+
+const RejectForm = styled(Form)`
+  display: inline-block;
+  width: 421px;
+
+  @media (max-width: 753px) {
+    width: 90%;
+  }
+`;
+
+const H2 = styled.h2`
+  font-size: 22px;
+  color: #292b37;
+  text-transform: uppercase;
+  margin: 67px 0 46px;
+`;
+const P = styled.p`
+  font-size: 16px;
+  line-height: 22px;
+  font-weight: 600;
+  color: ${ifProp("red", "#fb0327", "#292b37")};
+  margin: 30px 0;
+`;
