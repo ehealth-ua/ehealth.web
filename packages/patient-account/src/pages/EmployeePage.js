b37107a6881bdc6c9715312fb162a94bb7fa0811
ivchenkodima <ivchenkodima@gmail.com>
2018-06-22 12:29:19 +0300

    feat(patient-account): create declaration start flow with error mapping

--- packages/patient-account/src/pages/EmployeePage.js
+++ packages/patient-account/src/pages/EmployeePage.js
@@ -12,151 +12,210 @@ import DefinitionListView from "../components/DefinitionListView";
 import CreateDeclarationRequestMutation from "../graphql/CreateDeclarationRequestMutation.graphql";
 import EmployeeQuery from "../graphql/EmployeeQuery.graphql";
 import PersonQuery from "../graphql/PersonQuery.graphql";
+class EmployeePage extends React.Component {
+  state = {
+    error: false
+  };
 
-const EmployeePage = ({ match, history }) => (
-  <Query
-    query={EmployeeQuery}
-    variables={{ id: match.params.id }}
-    context={{ credentials: "same-origin" }}
-  >
-    {({ loading, error, data }) => {
-      const { employee } = data;
-      if (!employee) return null;
+  render() {
+    const { match, history } = this.props;
+    return (
+      <Query
+        query={EmployeeQuery}
+        variables={{ id: match.params.id }}
+        context={{ credentials: "same-origin" }}
+      >
+        {({ loading, error, data }) => {
+          const { employee } = data;
+          if (!employee) return null;
 
-      const { data: employeeData } = employee;
-      const {
-        party: {
-          specialities,
-          educations,
-          qualifications,
-          scienceDegree,
-          workingExperience,
-          aboutMyself
-        },
-        id: employee_id,
-        division: { id: division_id }
-      } = employeeData;
+          const { data: employeeData } = employee;
+          const {
+            party: {
+              specialities,
+              educations,
+              qualifications,
+              scienceDegree,
+              workingExperience,
+              aboutMyself
+            },
+            id: employee_id,
+            division: { id: division_id }
+          } = employeeData;
 
-      return (
-        <>
-          <Heading.H1>Крок 2. Відправте запит на декларацію</Heading.H1>
-          <DefinitionListSection>
-            <SubTitle>
-              {getFullName(employeeData.party)}
-              <Link onClick={() => history.goBack()} size="xs" upperCase>
-                Назад до результатів пошуку
-              </Link>
-            </SubTitle>
-            <DefinitionListView
-              labels={{
-                divisionName: "Назва відділення",
-                educations: "Освіта",
-                qualifications: "Кваліфікація",
-                scienceDegree: "Науковий ступінь",
-                specialities: "Спеціалізація",
-                workingExperience: "Стаж роботи",
-                aboutMyself: "Про себе"
-              }}
-              data={{
-                divisionName: employeeData.division.name,
-                educations: educations.map((item, i) => (
-                  <Text key={i}>
-                    <Text>{item.speciality}</Text>
-                    <Text>{item.institutionName}</Text>
-                    <Text>
-                      <DictionaryValue
-                        name="EDUCATION_DEGREE"
-                        item={item.degree}
-                      />
-                      <br />
-                      Диплом №{item.diplomaNumber} від {item.issuedDate}
-                    </Text>
-                    <Text>
-                      <DictionaryValue name="COUNTRY" item={item.country} />,{" "}
-                      {item.city}
-                    </Text>
-                  </Text>
-                )),
-                qualifications: qualifications.map((item, i) => (
-                  <Text key={i}>
-                    <Text>{item.speciality}</Text>
-                    <Text>{item.certificateNumber}</Text>
-                    <Text>
-                      <DictionaryValue
-                        name="QUALIFICATION_TYPE"
-                        item={item.type}
-                      />
-                      <br />
-                      Сертифікат №{item.diplomaNumber} від {item.issuedDate}
-                    </Text>
-                  </Text>
-                )),
-                scienceDegree: (
-                  <Text>
-                    <Text>
-                      <DictionaryValue
-                        name="SPECIALITY_TYPE"
-                        item={scienceDegree.speciality}
-                      />
-                    </Text>
+          return (
+            <>
+              <Heading.H1>Крок 2. Відправте запит на декларацію</Heading.H1>
+              <DefinitionListSection>
+                <SubTitle>
+                  {getFullName(employeeData.party)}
+                  <Link onClick={() => history.goBack()} size="xs" upperCase>
+                    Назад до результатів пошуку
+                  </Link>
+                </SubTitle>
+                <DefinitionListView
+                  labels={{
+                    divisionName: "Назва відділення",
+                    educations: "Освіта",
+                    qualifications: "Кваліфікація",
+                    scienceDegree: "Науковий ступінь",
+                    specialities: "Спеціалізація",
+                    workingExperience: "Стаж роботи",
+                    aboutMyself: "Про себе"
+                  }}
+                  data={{
+                    divisionName: employeeData.division.name,
+                    educations: educations.map((item, i) => (
+                      <Text key={i}>
+                        <Text>{item.speciality}</Text>
+                        <Text>{item.institutionName}</Text>
+                        <Text>
+                          <DictionaryValue
+                            name="EDUCATION_DEGREE"
+                            item={item.degree}
+                          />
+                          <br />
+                          Диплом №{item.diplomaNumber} від {item.issuedDate}
+                        </Text>
+                        <Text>
+                          <DictionaryValue name="COUNTRY" item={item.country} />,{" "}
+                          {item.city}
+                        </Text>
+                      </Text>
+                    )),
+                    qualifications: qualifications.map((item, i) => (
+                      <Text key={i}>
+                        <Text>{item.speciality}</Text>
+                        <Text>{item.certificateNumber}</Text>
+                        <Text>
+                          <DictionaryValue
+                            name="QUALIFICATION_TYPE"
+                            item={item.type}
+                          />
+                          <br />
+                          Сертифікат №{item.diplomaNumber} від {item.issuedDate}
+                        </Text>
+                      </Text>
+                    )),
+                    scienceDegree: (
+                      <Text>
+                        <Text>
+                          <DictionaryValue
+                            name="SPECIALITY_TYPE"
+                            item={scienceDegree.speciality}
+                          />
+                        </Text>
 
-                    <Text>{scienceDegree.institutionName}</Text>
-                    <Text>
-                      <DictionaryValue
-                        name="SCIENCE_DEGREE"
-                        item={scienceDegree.degree}
-                      />{" "}
-                      <br />
-                      Диплом №{scienceDegree.diplomaNumber} від{" "}
-                      {scienceDegree.issuedDate}
-                    </Text>
-                    <Text>
-                      <DictionaryValue
-                        name="COUNTRY"
-                        item={scienceDegree.country}
-                      />{" "}
-                      , {scienceDegree.city}
-                    </Text>
-                  </Text>
-                ),
-                workingExperience,
-                aboutMyself
-              }}
-            />
-          </DefinitionListSection>
-          <Query query={PersonQuery}>
-            {({ loading, error, data }) => {
-              if (!data.person) return null;
-              const { data: { id: person_id } } = data.person;
-              return (
-                <Mutation mutation={CreateDeclarationRequestMutation}>
-                  {(createDeclarationRequest, { loading, error }) => (
-                    <Button
-                      onClick={() =>
-                        createDeclarationRequest({
-                          variables: {
-                            input: {
-                              person_id,
-                              employee_id,
-                              division_id
-                              // scope: "family_doctor"
-                            }
-                          }
-                        })
-                      }
-                    >
-                      Відправити запит на декларацію
-                    </Button>
-                  )}
-                </Mutation>
-              );
-            }}
-          </Query>
-        </>
-      );
-    }}
-  </Query>
-);
+                        <Text>{scienceDegree.institutionName}</Text>
+                        <Text>
+                          <DictionaryValue
+                            name="SCIENCE_DEGREE"
+                            item={scienceDegree.degree}
+                          />{" "}
+                          <br />
+                          Диплом №{scienceDegree.diplomaNumber} від{" "}
+                          {scienceDegree.issuedDate}
+                        </Text>
+                        <Text>
+                          <DictionaryValue
+                            name="COUNTRY"
+                            item={scienceDegree.country}
+                          />{" "}
+                          , {scienceDegree.city}
+                        </Text>
+                      </Text>
+                    ),
+                    workingExperience,
+                    aboutMyself
+                  }}
+                />
+              </DefinitionListSection>
+              <Query query={PersonQuery}>
+                {({ loading, error, data }) => {
+                  if (!data.person) return null;
+                  const { data: { id: person_id } } = data.person;
+                  return (
+                    <Mutation mutation={CreateDeclarationRequestMutation}>
+                      {createDeclarationRequest => (
+                        <>
+                          <Heading.H3>
+                            <RedText>{this.state.error}</RedText>
+                          </Heading.H3>
+                          <Block>
+                            <Button
+                              disabled={this.state.error}
+                              onClick={async () => {
+                                try {
+                                  const variables = {
+                                    input: {
+                                      person_id,
+                                      employee_id,
+                                      division_id
+                                    }
+                                  };
+                                  const {
+                                    data
+                                  } = await createDeclarationRequest({
+                                    variables
+                                  });
+                                  console.log(data);
+                                } catch (e) {
+                                  const {
+                                    networkError: { result: { error } } = {}
+                                  } = e;
+                                  if (error) {
+                                    this.declarationErrorHandler(
+                                      error.invalid[0].rules[0].description
+                                    );
+                                  }
+                                }
+                              }}
+                            >
+                              Відправити запит на декларацію
+                            </Button>
+                          </Block>
+                        </>
+                      )}
+                    </Mutation>
+                  );
+                }}
+              </Query>
+            </>
+          );
+        }}
+      </Query>
+    );
+  }
+  declarationErrorHandler(message) {
+    switch (message) {
+      case "Employee does not belong to legal entity.":
+        this.setState({
+          error:
+            "Неможливо підписати декларацію. Лікар на разі не влаштований до лікарні."
+        });
+        break;
+      case "Doctor speciality does not meet the patient's age requirement.":
+        this.setState({
+          error: "Спеціальність лікаря не відповідає вашому віковому діапазону."
+        });
+        break;
+      case "Employee's speciality does not belong to a doctor: THERAPIST, PEDIATRICIAN, FAMILY_DOCTOR":
+        this.setState({
+          error:
+            "Спеціальність працівника не належить до списку: терапевт, педіатр, сімейний лікар"
+        });
+        break;
+      default:
+        this.setState({
+          error: "Щось пішло не так. Спробуйте обрати іншого лікаря."
+        });
+    }
+  }
+}
+
+const Block = styled.div`
+  padding: 20px 0;
+`;
 
 const DefinitionListSection = styled.div`
   margin: 30px 0;
@@ -179,6 +238,10 @@ const Text = styled.div`
   &:first-child {
     margin-top: 0;
   }
+`;
+
+const RedText = styled.div`
+  color: red;
 `;
 
 export default withRouter(EmployeePage);
