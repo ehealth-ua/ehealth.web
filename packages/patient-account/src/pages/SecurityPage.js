c9bf8d4e7ce9574fbca547ffe1e86816f9e533a0
andriy-ilin <andriy.ilin90@gmail.com>
2018-07-05 16:05:20 +0300

    fix(patient-account): add diffrent senarios to 2FA

--- packages/patient-account/src/pages/SecurityPage.js
+++ packages/patient-account/src/pages/SecurityPage.js
@@ -10,6 +10,7 @@ import AuthenticationFactorQuery from "../graphql/AuthenticationFactorQuery.grap
 import { prop } from "styled-tools";
 import {
   REACT_APP_UPDATE_FACTOR_URL,
+  REACT_APP_OAUTH_URL,
   REACT_APP_CLIENT_ID,
   REACT_APP_OAUTH_REDIRECT_URI
 } from "../env";
@@ -18,30 +19,51 @@ const SecurityPage = () => (
   <Query query={AuthenticationFactorQuery}>
     {({ loading, error, data }) => {
       if (loading || error || !data.factor) return null;
-      const { factor: { data: [{ factor }] } } = data;
+      const { factor: { data: factor } } = data;
+      let isActive, phone;
+      if (factor.length > 0) {
+        [{ isActive, factor: phone }] = factor;
+      }
+
       return (
         <>
           <Heading.H1>Безпека</Heading.H1>
           <Section>
-            <Heading.H3 weight="bold">Двоетапна перевірка</Heading.H3>
-            <DefinitionListView
-              labels={{
-                phone: "Номер телефону"
-              }}
-              data={{
-                phone: (
-                  <Link
-                    href={`${REACT_APP_UPDATE_FACTOR_URL}/?client_id=${REACT_APP_CLIENT_ID}&redirect_uri=${REACT_APP_OAUTH_REDIRECT_URI}`}
-                    size="s"
-                    upperCase
-                    color="black"
-                    icon={<PencilIcon height="14" />}
-                  >
-                    {factor}
-                  </Link>
-                )
-              }}
-            />
+            <Heading.H3 weight="bold">
+              Двоетапна перевірка{" "}
+              {(factor.length === 0 || !isActive) && "не встановлена"}
+            </Heading.H3>
+            {factor.length > 0 && isActive && phone ? (
+              <DefinitionListView
+                labels={{
+                  phone: "Номер телефону"
+                }}
+                data={{
+                  phone: (
+                    <Link
+                      href={`${REACT_APP_UPDATE_FACTOR_URL}/?client_id=${REACT_APP_CLIENT_ID}&redirect_uri=${REACT_APP_OAUTH_REDIRECT_URI}`}
+                      size="s"
+                      upperCase
+                      color={"black"}
+                      icon={<PencilIcon height="14" />}
+                    >
+                      {phone}
+                    </Link>
+                  )
+                }}
+              />
+            ) : (
+              <p>
+                На жаль, другий фактор авторизації був скинутий.<br />
+                Для того, щоб його задати повторно необхідно ще раз пройти
+                процес{" "}
+                <Link
+                  href={`${REACT_APP_OAUTH_URL}/?client_id=${REACT_APP_CLIENT_ID}&redirect_uri=${REACT_APP_OAUTH_REDIRECT_URI}`}
+                >
+                  авторизації
+                </Link>
+              </p>
+            )}
           </Section>
         </>
       );
