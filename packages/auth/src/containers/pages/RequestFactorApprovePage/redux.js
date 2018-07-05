f2bc0f643fff1aea83c03144245ee697363e2e95
ivchenkodima <ivchenkodima@gmail.com>
2018-07-05 10:50:29 +0300

    feat(auth): add new cabinet flow in approve-factor page

--- packages/auth/src/containers/pages/RequestFactorApprovePage/redux.js
+++ packages/auth/src/containers/pages/RequestFactorApprovePage/redux.js
@@ -4,6 +4,7 @@ import { getLocation } from "../../../reducers";
 import { approveFactor } from "../../../redux/factors";
 import { login } from "../../../redux/session";
 import { authorize } from "../../../redux/auth";
+import { REACT_APP_CLIENT_ID } from "../../../env";
 
 export const onSubmit = ({ code }) => (dispatch, getState) =>
   dispatch(approveFactor(parseInt(code, 10))).then(action => {
@@ -35,30 +36,28 @@ export const onSubmit = ({ code }) => (dispatch, getState) =>
           pathname: "/invite/accept"
         })
       );
-    }
-    // there is no flag for meta.next_step for patient-account
-    // so this is not working for admin, mitril, uadressess
-    // return dispatch(push({ ...location, pathname: "/accept" }) );
-
-    return dispatch(
-      authorize({
-        clientId: location.query.client_id,
-        redirectUri: location.query.redirect_uri
-      })
-    ).then(({ payload, error }) => {
-      console.log(payload, error);
-      if (error) {
-        switch (payload.response.error.message) {
-          case "The redirection URI provided does not match a pre-registered value.":
-            return dispatch(push(`/sign-in/failure/wrong_url`));
-          case "Invalid client id.":
-            return dispatch(push(`/sign-in/failure/invalid_client_id`));
-          case "User blocked.":
-            return dispatch(push(`/sign-in/failure/access_denied`));
-          default:
-            dispatch(push(`/sign-in/failure`));
+    } else if (location.query.client_id === REACT_APP_CLIENT_ID) {
+      return dispatch(
+        authorize({
+          clientId: location.query.client_id,
+          redirectUri: location.query.redirect_uri
+        })
+      ).then(({ payload, error }) => {
+        if (error) {
+          switch (payload.response.error.message) {
+            case "The redirection URI provided does not match a pre-registered value.":
+              return dispatch(push(`/sign-in/failure/wrong_url`));
+            case "Invalid client id.":
+              return dispatch(push(`/sign-in/failure/invalid_client_id`));
+            case "User blocked.":
+              return dispatch(push(`/sign-in/failure/access_denied`));
+            default:
+              dispatch(push(`/sign-in/failure`));
+          }
         }
-      }
-      return window && (window.location = payload.headers.get("location"));
-    });
+        return window && (window.location = payload.headers.get("location"));
+      });
+    } else {
+      return dispatch(push({ ...location, pathname: "/accept" }));
+    }
   });
