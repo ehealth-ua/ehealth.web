238fb95101902927962ef08736cf66d09697f30e
ephor <ephordnb@gmail.com>
2018-07-10 09:35:39 +0300

    test(utils): add test to stringifySearchParams

---
+++ packages/utils/src/__tests__/stringifySearchParams.js
@@ -0,0 +1,34 @@
+import stringifySearchParams from "../stringifySearchParams";
+
+describe("stringifySearchParams", () => {
+  it("should return query string from object", () => {
+    expect(
+      stringifySearchParams({
+        settlement: "Kyiv",
+        settlementType: "VILLAGE",
+        region: "Kyivskya oblast"
+      })
+    ).toBe("settlement=Kyiv&settlementType=VILLAGE&region=Kyivskya+oblast");
+  });
+  it("must pass through an object with incomplete data", () => {
+    expect(
+      stringifySearchParams({
+        settlement: "Kyiv",
+        settlementType: null,
+        region: null
+      })
+    ).toBe("settlement=Kyiv");
+  });
+  it("must pass through an object with empty strings", () => {
+    expect(
+      stringifySearchParams({
+        settlement: "Kyiv",
+        settlementType: "",
+        region: ""
+      })
+    ).toBe("settlement=Kyiv&settlementType=&region=");
+  });
+  it("should pass through empty object", () => {
+    expect(stringifySearchParams({})).toBe("");
+  });
+});
