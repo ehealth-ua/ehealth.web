555472aca1171685677966f22b6a3e3a8a2bad59
ephor <ephordnb@gmail.com>
2018-07-11 14:25:27 +0300

    test(utils): add negative test to fieldNameNormalizer and fieldNameDenormalizer

--- packages/utils/src/__tests__/fieldNameDenormalizer.js
+++ packages/utils/src/__tests__/fieldNameDenormalizer.js
@@ -1,7 +1,12 @@
 import fieldNameDenormalizer from "../fieldNameDenormalizer";
+import fieldNameNormalizer from "../fieldNameNormalizer";
 
 describe("fieldNameDenormalizer", () => {
   it("should convert camelCase to snake_case", () => {
     expect(fieldNameDenormalizer("someTextFromDb")).toBe("some_text_from_db");
+  });
+
+  it("shouldn't convert something that is not camelCase to snake_case", () => {
+    expect(fieldNameNormalizer("SOME_TEXT")).toBe("SOME_TEXT");
   });
 });
--- packages/utils/src/__tests__/fieldNameNormalizer.js
+++ packages/utils/src/__tests__/fieldNameNormalizer.js
@@ -4,4 +4,8 @@ describe("fieldNameNormalizer", () => {
   it("should convert snake_case to camelCase", () => {
     expect(fieldNameNormalizer("some_text_from_db")).toBe("someTextFromDb");
   });
+
+  it("shouldn't convert something that is not snake_case to camelCase", () => {
+    expect(fieldNameNormalizer("SOME_TEXT")).toBe("SOME_TEXT");
+  });
 });
