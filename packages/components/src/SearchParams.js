e769578b79eb1928a01b88185e241e57d3d465d0
ephor <ephordnb@gmail.com>
2018-07-25 15:03:16 +0300

    feat(components): add setSearchParamsImmediate to SearchParams component

--- packages/components/src/SearchParams.js
+++ packages/components/src/SearchParams.js
@@ -1,10 +1,26 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import { parseSearchParams } from "@ehealth/utils";
+import { parseSearchParams, stringifySearchParams } from "@ehealth/utils";
+import compose from "recompose/compose";
+import isEqual from "lodash/isEqual";
+
+const setSearchParams = (params, method, location, history) => {
+  const search = compose(stringifySearchParams, parseSearchParams)(
+    location.search
+  );
+  if (isEqual(stringifySearchParams(params), search)) return null;
+  history[method]({ ...location, search: stringifySearchParams(params) });
+};
 
 const SearchParams = ({ children, render = children }) => (
   <Route
-    render={({ location: { search } }) => render(parseSearchParams(search))}
+    render={({ location, history }) => {
+      return render({
+        searchParams: parseSearchParams(location.search),
+        setSearchParamsImmediate: (params, method = "replace") =>
+          setSearchParams(params, method, location, history)
+      });
+    }}
   />
 );
 
