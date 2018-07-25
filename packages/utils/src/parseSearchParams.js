416f81af3a652c1ca5ee33c7f5074e210721c0ef
ephor <ephordnb@gmail.com>
2018-07-25 14:57:47 +0300

    refactor(utils): add unflatten to parseSearchParams

--- packages/utils/src/parseSearchParams.js
+++ packages/utils/src/parseSearchParams.js
@@ -1,7 +1,10 @@
-const parseSearchParams = queryString =>
-  Array.from(new URLSearchParams(queryString).entries()).reduce(
-    (params, [name, value]) => ({ ...params, [name]: value }),
-    {}
-  );
+import { unflatten } from "flat";
+
+const parseSearchParams = queryString => {
+  const parseParams = Array.from(
+    new URLSearchParams(queryString).entries()
+  ).reduce((params, [name, value]) => ({ ...params, [name]: value }), {});
+  return unflatten(parseParams);
+};
 
 export default parseSearchParams;
