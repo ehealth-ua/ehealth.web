fc7a2b2fb0b569dbaac754e70f71b8fe53450f83
Myroslav Kiurchev <kiurchv@gmail.com>
2018-08-06 16:44:55 +0300

    feat(test-preset): add transforms

--- packages/test-preset/e2e/project/jest-preset.js
+++ packages/test-preset/e2e/project/jest-preset.js
@@ -1,7 +1,10 @@
 const cucumberPreset = require("jest-preset-cucumber");
 
+const transforms = require("../../transforms");
+
 module.exports = {
   setupTestFrameworkScriptFile: "expect-puppeteer",
   testEnvironment: "@ehealth/test-preset/e2e/environment",
-  ...cucumberPreset
+  ...cucumberPreset,
+  ...transforms
 };
--- packages/test-preset/unit/jest-preset.js
+++ packages/test-preset/unit/jest-preset.js
@@ -1,4 +1,8 @@
+const transforms = require("../transforms");
+
 module.exports = {
+  setupFiles: ["@ehealth/test-preset/polyfills"],
   testEnvironment: "@ehealth/test-preset/unit/environment",
-  testURL: "http://localhost" // temporary work around facebook/jest#6766
+  testURL: "http://localhost", // temporary work around facebook/jest#6766
+  ...transforms
 };
