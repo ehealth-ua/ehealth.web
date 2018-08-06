5e885f2da10cbfbafe2386f4c0fbf0141c88ece6
Myroslav Kiurchev <kiurchv@gmail.com>
2018-08-06 16:40:56 +0300

    fix(gatekeeper): add error middleware

--- packages/gatekeeper/server.js
+++ packages/gatekeeper/server.js
@@ -2,6 +2,7 @@ require("@ehealth/env/load");
 
 const url = require("url");
 const express = require("express");
+require("express-async-errors");
 const fetch = require("node-fetch");
 
 const {
@@ -21,59 +22,55 @@ const app = express();
 app.set("trust proxy", true);
 
 app.get("/auth/redirect", async (req, res) => {
-  try {
-    const { query: { code }, protocol, path } = req;
+  const {
+    query: { code },
+    protocol,
+    path
+  } = req;
 
-    const redirect_uri = url.format({
-      protocol,
-      host: req.get("host"),
-      pathname: path
-    });
+  const redirect_uri = url.format({
+    protocol,
+    host: req.get("host"),
+    pathname: path
+  });
 
-    const { value, user_id, expires_at } = await authenticate({
-      code,
-      redirect_uri
-    });
+  const { value, user_id, expires_at } = await authenticate({
+    code,
+    redirect_uri
+  });
 
-    const expires = new Date(expires_at * 1000);
-    const secure = NODE_ENV !== "development";
+  const expires = new Date(expires_at * 1000);
+  const secure = NODE_ENV !== "development";
 
-    res.cookie(AUTH_COOKIE_NAME, value, {
-      domain: COOKIE_DOMAIN,
-      expires,
-      secure,
-      httpOnly: true
-    });
+  res.cookie(AUTH_COOKIE_NAME, value, {
+    domain: COOKIE_DOMAIN,
+    expires,
+    secure,
+    httpOnly: true
+  });
 
-    res.cookie(META_COOKIE_NAME, user_id, {
-      domain: COOKIE_DOMAIN,
-      expires,
-      secure
-    });
+  res.cookie(META_COOKIE_NAME, user_id, {
+    domain: COOKIE_DOMAIN,
+    expires,
+    secure
+  });
 
-    res.redirect(REDIRECT_URL);
-  } catch (error) {
-    res.status(error.meta.code).json(error);
-  }
+  res.redirect(REDIRECT_URL);
 });
 
 app.get("/auth/logout", async (req, res) => {
-  try {
-    const secure = NODE_ENV !== "development";
+  const secure = NODE_ENV !== "development";
 
-    res.clearCookie(AUTH_COOKIE_NAME, {
-      domain: COOKIE_DOMAIN,
-      secure,
-      httpOnly: true
-    });
-    res.clearCookie(META_COOKIE_NAME, {
-      domain: COOKIE_DOMAIN,
-      secure
-    });
-    res.redirect(REDIRECT_URL);
-  } catch (error) {
-    res.status(error.meta.code).json(error);
-  }
+  res.clearCookie(AUTH_COOKIE_NAME, {
+    domain: COOKIE_DOMAIN,
+    secure,
+    httpOnly: true
+  });
+  res.clearCookie(META_COOKIE_NAME, {
+    domain: COOKIE_DOMAIN,
+    secure
+  });
+  res.redirect(REDIRECT_URL);
 });
 
 const authenticate = async ({ code, redirect_uri }) => {
@@ -96,6 +93,19 @@ const authenticate = async ({ code, redirect_uri }) => {
   if (error) throw { meta, error };
   return data;
 };
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error);
+
+  if (error instanceof Error) {
+    next(error);
+  } else {
+    const { code = 500 } = error.meta || {};
+    res.status(code).json(error);
+  }
+};
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Listening on http://0.0.0.0:${PORT}`);
