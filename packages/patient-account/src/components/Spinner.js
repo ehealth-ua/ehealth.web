2714157fba4fd83a627cb4df9779f0ea9824eb29
ephor <ephordnb@gmail.com>
2018-08-08 17:33:53 +0300

    feat(patient-account): add Spinner component

---
+++ packages/patient-account/src/components/Spinner.js
@@ -0,0 +1,10 @@
+import styled from "react-emotion/macro";
+
+import { SpinnerIcon } from "@ehealth/icons";
+
+const Spinner = styled(SpinnerIcon)`
+  display: flex;
+  margin: 10px auto;
+`;
+
+export default Spinner;
