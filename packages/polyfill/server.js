588a3dfa36dbf5758070ce3f19830e6c03198f79
andriy-ilin <andriy.ilin90@gmail.com>
2018-08-08 18:43:22 +0300

    fix(polyfill): update features in getPolyfillString

--- packages/polyfill/server.js
+++ packages/polyfill/server.js
@@ -1,28 +1,29 @@
-require("@ehealth/env");
+require("@ehealth/env/load");
 const url = require("url");
 const express = require("express");
+require("express-async-errors");
 const polyfill = require("polyfill-service");
 
-const { PORT = 4001 } = process.env;
+const { PORT } = process.env;
 
 const app = express();
 
-app.get("/polyfill.js", async (req, res) => {
-  try {
-    const script = await polyfill.getPolyfillString({
-      uaString: req.headers["user-agent"],
-      minify: true
-    });
-    res.set({
-      "Content-Type": "application/javascript;charset=utf-8",
-      "Content-Length": script.length
-    });
-    res.setHeader("Cache-Control", "immutable");
-    res.write(script);
-    res.end();
-  } catch (error) {
-    res.status(error.meta.code).json(error);
-  }
+app.get("/polyfill:min(.min)?.js", async (req, res) => {
+  const script = await polyfill.getPolyfillString({
+    uaString: req.headers["user-agent"],
+    minify: req.params.min === ".min",
+    features: {
+      default: {},
+      "Object.entries": {}
+    }
+  });
+
+  res.set({
+    "Content-Type": "application/javascript;charset=utf-8",
+    "Content-Length": script.length
+  });
+  res.write(script);
+  res.end();
 });
 
 app.listen(PORT, () => {
