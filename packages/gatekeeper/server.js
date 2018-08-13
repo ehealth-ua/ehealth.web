1c8a6f60536458ea74ab8acd6f57b83da1b81757
ivchenkodima <ivchenkodima@gmail.com>
2018-08-13 15:03:23 +0300

    fix(gatekeeper): remove lines after rebase

--- packages/gatekeeper/server.js
+++ packages/gatekeeper/server.js
@@ -22,7 +22,6 @@ const app = express();
 app.set("trust proxy", true);
 
 app.get("/auth/redirect", async (req, res) => {
-<<<<<<< HEAD
   const {
     query: { code },
     protocol,
@@ -57,51 +56,6 @@ app.get("/auth/redirect", async (req, res) => {
   });
 
   res.redirect(REDIRECT_URL);
-=======
-  try {
-    const { query: { code }, protocol, path } = req;
-
-    const redirect_uri = url.format({
-      protocol,
-      host: req.get("host"),
-      pathname: path
-    });
-
-    const {
-      value,
-      user_id,
-      expires_at,
-      details: { scope }
-    } = await authenticate({
-      code,
-      redirect_uri
-    });
-
-    const expires = new Date(expires_at * 1000);
-    const secure = NODE_ENV !== "development";
-
-    res.cookie(AUTH_COOKIE_NAME, value, {
-      domain: COOKIE_DOMAIN,
-      expires,
-      secure,
-      httpOnly: true
-    });
-    const META_DATA = {
-      scope,
-      user_id
-    };
-
-    res.cookie(META_COOKIE_NAME, JSON.stringify(META_DATA), {
-      domain: COOKIE_DOMAIN,
-      expires,
-      secure
-    });
-
-    res.redirect(REDIRECT_URL);
-  } catch (error) {
-    res.status(error.meta.code).json(error);
-  }
->>>>>>> feat(admin-legacy): migrate ngs.admin to ehealth.web
 });
 
 app.get("/auth/logout", async (req, res) => {
