180949a7400b5d83f7cc4badb31c6ba1cd8c77c0
ivchenkodima <ivchenkodima@gmail.com>
2018-08-15 16:15:39 +0300

    fix(gatekeeper): revert to right logic

--- packages/gatekeeper/server.js
+++ packages/gatekeeper/server.js
@@ -22,40 +22,47 @@ const app = express();
 app.set("trust proxy", true);
 
 app.get("/auth/redirect", async (req, res) => {
-  const {
-    query: { code },
-    protocol,
-    path
-  } = req;
-
-  const redirect_uri = url.format({
-    protocol,
-    host: req.get("host"),
-    pathname: path
-  });
-
-  const { value, user_id, expires_at } = await authenticate({
-    code,
-    redirect_uri
-  });
-
-  const expires = new Date(expires_at * 1000);
-  const secure = NODE_ENV !== "development";
-
-  res.cookie(AUTH_COOKIE_NAME, value, {
-    domain: COOKIE_DOMAIN,
-    expires,
-    secure,
-    httpOnly: true
-  });
-
-  res.cookie(META_COOKIE_NAME, user_id, {
-    domain: COOKIE_DOMAIN,
-    expires,
-    secure
-  });
-
-  res.redirect(REDIRECT_URL);
+  try {
+    const {
+      query: { code },
+      protocol,
+      path
+    } = req;
+    const redirect_uri = url.format({
+      protocol,
+      host: req.get("host"),
+      pathname: path
+    });
+    const {
+      value,
+      user_id,
+      expires_at,
+      details: { scope }
+    } = await authenticate({
+      code,
+      redirect_uri
+    });
+    const expires = new Date(expires_at * 1000);
+    const secure = NODE_ENV !== "development";
+    res.cookie(AUTH_COOKIE_NAME, value, {
+      domain: COOKIE_DOMAIN,
+      expires,
+      secure,
+      httpOnly: true
+    });
+    const META_DATA = {
+      scope,
+      user_id
+    };
+    res.cookie(META_COOKIE_NAME, JSON.stringify(META_DATA), {
+      domain: COOKIE_DOMAIN,
+      expires,
+      secure
+    });
+    res.redirect(REDIRECT_URL);
+  } catch (error) {
+    res.status(error.meta.code).json(error);
+  }
 });
 
 app.get("/auth/logout", async (req, res) => {
