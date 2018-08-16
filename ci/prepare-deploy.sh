49dff0f76d79a910560a639907e9505d319d4058
Myroslav Kiurchev <kiurchv@gmail.com>
2018-08-16 12:28:25 +0300

    ci: add missing git credential config before deploy

--- ci/prepare-deploy.sh
+++ ci/prepare-deploy.sh
@@ -3,5 +3,6 @@ openssl aes-256-cbc -K $encrypted_d422cd97315f_key -iv $encrypted_d422cd97315f_i
 gcloud auth activate-service-account --key-file=$TRAVIS_BUILD_DIR/eHealth-d14f79f426fb.json
 gcloud container clusters get-credentials dev --zone europe-west1-d --project ehealth-162117
 curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
+git config --global credential.helper "store"
 echo "https://${GITHUB_TOKEN}:@github.com" > $HOME/.git-credentials
 git clone https://github.com/edenlabllc/ehealth.charts.git charts
