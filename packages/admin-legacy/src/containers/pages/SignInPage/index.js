25c7778f23dc7203ef0af0ecf80beadc55d3142b
andriy-ilin <andriy.ilin90@gmail.com>
2018-08-20 10:54:34 +0300

    fix(admin-legacy): delete string scope from link on SignIn

--- packages/admin-legacy/src/containers/pages/SignInPage/index.js
+++ packages/admin-legacy/src/containers/pages/SignInPage/index.js
@@ -15,7 +15,9 @@ import styles from "./styles.module.css";
 
 class SignInPage extends React.Component {
   render() {
-    const { location: { query } } = this.props;
+    const {
+      location: { query }
+    } = this.props;
 
     return (
       <section className={styles.main} id="sign-in-page">
@@ -34,7 +36,7 @@ class SignInPage extends React.Component {
           <article className={styles.form}>
             <a
               className={styles.button}
-              href={`${REACT_APP_OAUTH_URL}?client_id=${REACT_APP_CLIENT_ID}&scope=${REACT_APP_SCOPES}&redirect_uri=${REACT_APP_OAUTH_REDIRECT_URI}`}
+              href={`${REACT_APP_OAUTH_URL}?client_id=${REACT_APP_CLIENT_ID}&redirect_uri=${REACT_APP_OAUTH_REDIRECT_URI}`}
             >
               Увійти за допомогою EHEALTH
             </a>
