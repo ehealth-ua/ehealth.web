7f5abfb743eee93705415e92aaaafc4959b1426e
ivchenkodima <ivchenkodima@gmail.com>
2018-08-21 15:45:46 +0300

    feat(admin): add colors to theme

--- packages/admin/src/theme.js
+++ packages/admin/src/theme.js
@@ -1,3 +1,36 @@
-const theme = {};
+const theme = {
+  colors: {
+    black: "#000",
+
+    white: "#fff",
+
+    disabled: "#E9EDF1",
+    disabledBorder: "#DFE3E9",
+    font: {
+      red: "#ED1C24",
+      blue: "#2EA2F8",
+      carbon: "#354052",
+      green: "#1BB934"
+    },
+
+    gray: "#DFE1E5",
+    darken: "#C2CAD4",
+
+    green: ["#39B54A", "#1E930B", "#1A740B", "#1E930B"],
+    greenBorder: ["#249533", "#1F872D", "#14661F", "#249533"],
+
+    blue: ["#1991EB", "#1584D7", "#0C73C1", "#1584D7"],
+    blueBorder: ["#1585D8", "#1578C2", "#1468A7", "#1578C2"],
+
+    red: ["#F85359", "#F14D53", "#EC4349", "#F14D53"],
+    redBorder: ["#DB161E", "#D0121A", "#D01119", "#D0121A"],
+
+    carbon: ["#516173", "#415061", "#354353", "#415061"],
+    carbonBorder: ["#3B4958", "#334050", "#303C4B", "#334050"],
+
+    orange: ["#F7981C", "#EF9219", "#E88B12", "#EF9219"],
+    orangeBorder: ["#F36A19", "#EA6211", "#E25D0E", "#EA6211"]
+  }
+};
 
 export default theme;
