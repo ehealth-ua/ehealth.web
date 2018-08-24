57a46b43a42e0da8a0de325eb3f954e69afc52e5
Myroslav Kiurchev <kiurchv@gmail.com>
2018-08-24 15:44:36 +0300

    refactor(gatekeeper): remove redundant try block

--- packages/gatekeeper/server.js
+++ packages/gatekeeper/server.js
@@ -22,47 +22,41 @@ const app = express();
 app.set("trust proxy", true);
 
 app.get("/auth/redirect", async (req, res) => {
-  try {
-    const {
-      query: { code },
-      protocol,
-      path
-    } = req;
-    const redirect_uri = url.format({
-      protocol,
-      host: req.get("host"),
-      pathname: path
-    });
-    const {
-      value,
-      user_id,
-      expires_at,
-      details: { scope }
-    } = await authenticate({
-      code,
-      redirect_uri
-    });
-    const expires = new Date(expires_at * 1000);
-    const secure = NODE_ENV !== "development";
-    res.cookie(AUTH_COOKIE_NAME, value, {
-      domain: COOKIE_DOMAIN,
-      expires,
-      secure,
-      httpOnly: true
-    });
-    const META_DATA = {
-      scope,
-      user_id
-    };
-    res.cookie(META_COOKIE_NAME, JSON.stringify(META_DATA), {
-      domain: COOKIE_DOMAIN,
-      expires,
-      secure
-    });
-    res.redirect(REDIRECT_URL);
-  } catch (error) {
-    res.status(error.meta.code).json(error);
-  }
+  const {
+    protocol,
+    path: pathname,
+    headers: { host },
+    query: { code }
+  } = req;
+
+  const redirect_uri = url.format({ protocol, host, pathname });
+
+  const {
+    value,
+    user_id,
+    expires_at,
+    details: { scope }
+  } = await authenticate({ code, redirect_uri });
+
+  const expires = new Date(expires_at * 1000);
+  const secure = NODE_ENV !== "development";
+
+  res.cookie(AUTH_COOKIE_NAME, value, {
+    domain: COOKIE_DOMAIN,
+    expires,
+    secure,
+    httpOnly: true
+  });
+
+  const metadata = { scope, userId: user_id };
+
+  res.cookie(META_COOKIE_NAME, JSON.stringify(metadata), {
+    domain: COOKIE_DOMAIN,
+    expires,
+    secure
+  });
+
+  res.redirect(REDIRECT_URL);
 });
 
 app.get("/auth/logout", async (req, res) => {
@@ -73,10 +67,12 @@ app.get("/auth/logout", async (req, res) => {
     secure,
     httpOnly: true
   });
+
   res.clearCookie(META_COOKIE_NAME, {
     domain: COOKIE_DOMAIN,
     secure
   });
+
   res.redirect(REDIRECT_URL);
 });
 
