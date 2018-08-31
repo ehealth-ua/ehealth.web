3eac1737ec67877332939b2061d5e83b40649f2d
ephor <ephordnb@gmail.com>
2018-08-31 11:53:05 +0300

    feat(admin): tooltip component

---
+++ packages/admin/src/components/Tooltip.js
@@ -0,0 +1,114 @@
+import React from "react";
+import styled from "react-emotion/macro";
+import { css } from "react-emotion";
+import { Manager, Reference, Popper } from "react-popper";
+
+const Tooltip = ({ content, component: Component }) => (
+  <Manager>
+    <Reference>
+      {({ ref }) => (
+        <Target innerRef={ref}>
+          <Component />
+        </Target>
+      )}
+    </Reference>
+
+    <Popper
+      placement="auto"
+      positionFixed
+      modifiers={{
+        flip: {
+          behavior: ["left", "bottom", "top"]
+        },
+        preventOverflow: {
+          padding: 20,
+          boundariesElement: "viewport"
+        }
+      }}
+    >
+      {({ ref, style, placement, arrowProps, ...props }) => (
+        <TooltipWrapper style={style} innerRef={ref} placement={placement}>
+          {content}
+          <Arrow
+            style={{
+              top: arrowProps.style.top ? `${arrowProps.style.top}px` : "auto",
+              left: arrowProps.style.left
+                ? `${arrowProps.style.left}px`
+                : "auto"
+            }}
+            placement={placement}
+            innerRef={arrowProps.ref}
+          />
+        </TooltipWrapper>
+      )}
+    </Popper>
+  </Manager>
+);
+
+const TooltipWrapper = styled.div`
+  max-width: 450px;
+  margin: 7px;
+  padding: 10px;
+  background-image: linear-gradient(0deg, #f2f4f7 0%, #ffffff 100%);
+  border: 1px solid #ced0da;
+  border-radius: 2px;
+  font-size: 14px;
+  color: #354052;
+  text-align: center;
+  line-height: 19px;
+  opacity: 0;
+  visibility: hidden;
+  transition: all 100ms;
+`;
+
+const Target = styled.div`
+  display: inline-block;
+  &:hover + ${TooltipWrapper} {
+    opacity: 1;
+    visibility: visible;
+  }
+`;
+
+const arrowPosition = ({ placement }) => {
+  switch (placement) {
+    case "top":
+      return css`
+        bottom: 0;
+        transform: rotate(-135deg);
+        clip-path: polygon(100% 0, 0 100%, 0 0);
+      `;
+    case "right":
+      return css`
+        right: 100%;
+        transform: rotate(-135deg);
+        clip-path: polygon(100% 100%, 0 0, 100% 0);
+      `;
+    case "bottom":
+      return css`
+        bottom: 100%;
+        transform: rotate(-135deg);
+        clip-path: polygon(100% 0%, 0% 100%, 100% 100%);
+      `;
+    case "left":
+      return css`
+        right: 0;
+        transform: rotate(135deg);
+        clip-path: polygon(0 0, 0 100%, 100% 0);
+      `;
+    default:
+      return;
+  }
+};
+
+const Arrow = styled.div`
+  position: absolute;
+  margin: -6px;
+  ${arrowPosition};
+  width: 12px;
+  height: 12px;
+  background-image: linear-gradient(135deg, #f2f4f7 0%, #ffffff 100%);
+  border: 1px solid #ced0da;
+  will-change: transform;
+`;
+
+export default Tooltip;
