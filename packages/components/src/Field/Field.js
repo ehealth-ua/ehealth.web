8f2eb6d0c6b412aef0e667bb5a3222ae2ae4e442
Myroslav Kiurchev <kiurchv@gmail.com>
2018-09-04 13:51:42 +0300

    feat(components): annotate Field component, add field state to render prop metadata

--- packages/components/src/Field/Field.js
+++ packages/components/src/Field/Field.js
@@ -1,22 +1,55 @@
-import React from "react";
-import { Field as FinalFormField } from "react-final-form";
+// @flow
+import * as React from "react";
+import * as FinalForm from "react-final-form";
 import { pickProps } from "@ehealth/utils";
 
 const FINAL_FORM_FIELD_PROPS = [
   "allowNull",
   "format",
+  "formatOnBlur",
   "isEqual",
   "name",
   "parse",
+  "validate",
+  "validateFields",
   "subscription",
   "value"
 ];
 
-const Field = ({ children, render = children, type, ...props }) => {
+const FIELD_STATES = ["disabled", "errored", "active"];
+
+type FieldState = "disabled" | "errored" | "active";
+
+type FieldRenderProp = ({
+  input: $PropertyType<FinalForm.FieldRenderProps, "input">,
+  meta: $PropertyType<FinalForm.FieldRenderProps, "meta"> & {
+    state?: FieldState,
+    errored: boolean,
+    error?: string
+  }
+}) => React.Node;
+
+type FieldProps = {
+  children?: FieldRenderProp,
+  render?: FieldRenderProp,
+  type: string
+} & FinalForm.FieldProps;
+
+const Field = ({
+  children,
+  // $FlowFixMe https://github.com/facebook/flow/issues/6832
+  render = children,
+  type,
+  ...props
+}: FieldProps): React.Node => {
   const [fieldProps, inputProps] = pickProps(props, FINAL_FORM_FIELD_PROPS);
 
+  if (typeof render !== "function") {
+    return null;
+  }
+
   return (
-    <FinalFormField
+    <FinalForm.Field
       {...fieldProps}
       type={type}
       render={({ input, meta }) =>
@@ -24,6 +57,7 @@ const Field = ({ children, render = children, type, ...props }) => {
           input: { ...inputProps, ...input, type },
           meta: {
             ...meta,
+            state: getFieldState(inputProps, meta),
             errored: isErrored(meta),
             error: getError(meta)
           }
@@ -35,15 +69,32 @@ const Field = ({ children, render = children, type, ...props }) => {
 
 export default Field;
 
+const getFieldState = (
+  inputProps: { disabled?: boolean },
+  meta: $PropertyType<FinalForm.FieldRenderProps, "meta">
+): ?FieldState => {
+  const { disabled } = inputProps;
+  const { active } = meta;
+  const errored = isErrored(meta);
+
+  const flags = [disabled, errored, active];
+
+  return FIELD_STATES.find((_name, index) => flags[index]);
+};
+
 const isErrored = ({
   error,
   submitError,
   data = {},
   touched,
   dirtySinceLastSubmit
-}) =>
+}: $PropertyType<FinalForm.FieldRenderProps, "meta">): boolean =>
   (error && touched) ||
   ((submitError || data.submitError) && !dirtySinceLastSubmit);
 
-const getError = ({ error, submitError, data = {} }) =>
+const getError = ({
+  error,
+  submitError,
+  data = {}
+}: $PropertyType<FinalForm.FieldRenderProps, "meta">): string =>
   error || submitError || data.submitError;
