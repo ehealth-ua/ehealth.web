b611e6623d3ddda70e713fc033586a6e2c5b0b5d
Myroslav Kiurchev <kiurchv@gmail.com>
2018-09-05 16:28:40 +0300

    fix(admin): fix theme import

--- packages/admin/src/App.js
+++ packages/admin/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { ThemeProvider } from "@ehealth/components";
 
 import "./globalStyles";
-import theme from "./theme";
+import * as theme from "./theme";
 import ErrorBoundary from "./ErrorBoundary";
 import DataProvider from "./DataProvider";
 import Routes from "./Routes";
