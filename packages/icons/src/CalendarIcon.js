cc9766a78b066fe203d311400ad9efbffb1b2b8c
ephor <ephordnb@gmail.com>
2018-09-12 14:32:32 +0300

    fix(icons): change calendar icon

--- packages/icons/src/CalendarIcon.js
+++ packages/icons/src/CalendarIcon.js
@@ -1,11 +1,38 @@
 import React from "react";
 
 const CalendarIcon = props => (
-  <svg viewBox="0 0 26 25" {...props}>
-    <path
-      fill="#a0a4ab"
-      d="M14.8 10.1h-3.1v3.1h3.1v-3.1zm0 6.3h-3.1v3.1h3.1v-3.1zm-5.5-6.3H6.2v3.1h3.1v-3.1zm10.9 6.3h-3.1v3.1h3.1v-3.1zm0-6.3h-3.1v3.1h3.1v-3.1zM9.3 16.4H6.2v3.1h3.1v-3.1zM21.4 0H4.6C2.1 0 0 2.1 0 4.6v15.7C0 22.9 2.1 25 4.6 25h16.8c2.5 0 4.6-2.1 4.6-4.6V4.6C26 2.1 23.9 0 21.4 0zm2.7 20.4c0 1.5-1.2 2.7-2.7 2.7H4.6c-1.5 0-2.7-1.2-2.7-2.7V6.7H24v13.7h.1z"
-    />
+  <svg width="16px" height="16px" viewBox="0 0 16 16" {...props}>
+    <g
+      id="UI-Elements-Kit"
+      stroke="none"
+      strokeWidth="1"
+      fill="none"
+      fillRule="evenodd"
+    >
+      <g
+        id="White-Layout"
+        transform="translate(-1023.000000, -1860.000000)"
+        fill="#CED0DA"
+      >
+        <g
+          id="Dropdowns-&amp;-Selector"
+          transform="translate(187.000000, 1392.000000)"
+        >
+          <g id="Dropdowns" transform="translate(0.000000, 428.000000)">
+            <g id="Calendar" transform="translate(825.000000, 0.000000)">
+              <g id="Date-Picker" transform="translate(0.000000, 30.000000)">
+                <g id="Icon-+-Text" transform="translate(11.000000, 10.000000)">
+                  <path
+                    d="M4,9 L6,9 L6,7 L4,7 L4,9 Z M4,12 L6,12 L6,10 L4,10 L4,12 Z M7,9 L9,9 L9,7 L7,7 L7,9 Z M7,12 L9,12 L9,10 L7,10 L7,12 Z M10,9 L12,9 L12,7 L10,7 L10,9 Z M10,12 L12,12 L12,10 L10,10 L10,12 Z M14,6 C14,5.448 13.552,5 13,5 L3,5 C2.448,5 2,5.448 2,6 L2,13 C2,13.552 2.448,14 3,14 L13,14 C13.552,14 14,13.552 14,13 L14,6 Z M14,16 L2,16 C0.896,16 0,15.105 0,14 L0,2 C0,0.895 0.896,0 2,0 L4,0 L4,1 C4,1.552 4.448,2 5,2 C5.552,2 6,1.552 6,1 L6,0 L10,0 L10,1 C10,1.552 10.448,2 11,2 C11.552,2 12,1.552 12,1 L12,0 L14,0 C15.105,0 16,0.895 16,2 L16,14 C16,15.105 15.105,16 14,16 L14,16 Z"
+                    id="Calendar-Icon"
+                  />
+                </g>
+              </g>
+            </g>
+          </g>
+        </g>
+      </g>
+    </g>
   </svg>
 );
 
