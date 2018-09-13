f43f3a1f1433c97a9b92aa5ce565055b8c96592b
burlachenkovv <val.volkov@gmail.com>
2018-09-13 13:36:09 +0300

    refactor(admin): reuse UI components from Dropdown in the SelectList

--- packages/admin/src/components/SelectList.js
+++ packages/admin/src/components/SelectList.js
@@ -1,10 +1,9 @@
 import React from "react";
-import styled from "react-emotion/macro";
-import { ifProp } from "styled-tools";
+import Dropdown from "./Dropdown";
 
 const SelectList = React.forwardRef(
   ({ isOpen, itemsList, inputValue, getItemProps }, ref) => (
-    <Container innerRef={ref} isOpen={isOpen}>
+    <Dropdown.List innerRef={ref} isOpen={isOpen}>
       {isOpen
         ? itemsList
             .filter(
@@ -13,7 +12,7 @@ const SelectList = React.forwardRef(
                 item.value.toLowerCase().includes(inputValue.toLowerCase())
             )
             .map((item, index) => (
-              <Item
+              <Dropdown.Item
                 {...getItemProps({
                   key: item.value,
                   index,
@@ -21,36 +20,11 @@ const SelectList = React.forwardRef(
                 })}
               >
                 {item.value}
-              </Item>
+              </Dropdown.Item>
             ))
         : null}
-    </Container>
+    </Dropdown.List>
   )
 );
 
 export default SelectList;
-
-const Container = styled.ul`
-  padding: 0;
-  margin-top: 0;
-  position: absolute;
-  background-color: white;
-  width: 100%;
-  max-height: 20rem;
-  overflow-y: auto;
-  overflow-x: hidden;
-  outline: 0;
-  transition: opacity 0.1s ease;
-  border: ${ifProp("isOpen", "1px solid #dfe3e9", "none")};
-  border-top-width: 0;
-`;
-
-const Item = styled.li`
-  position: relative;
-  display: block;
-  text-align: left;
-  padding: 12px;
-  &:hover {
-    background-color: #e9edf1;
-  }
-`;
