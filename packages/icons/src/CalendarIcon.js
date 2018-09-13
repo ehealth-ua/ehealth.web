14129689ad675111a98089919c8963e3fd172f3b
ephor <ephordnb@gmail.com>
2018-09-13 16:29:46 +0300

    fix(icon): optimize calendar icon

--- packages/icons/src/CalendarIcon.js
+++ packages/icons/src/CalendarIcon.js
@@ -1,38 +1,11 @@
 import React from "react";
 
 const CalendarIcon = props => (
-  <svg width="16px" height="16px" viewBox="0 0 16 16" {...props}>
-    <g
-      id="UI-Elements-Kit"
-      stroke="none"
-      strokeWidth="1"
-      fill="none"
-      fillRule="evenodd"
-    >
-      <g
-        id="White-Layout"
-        transform="translate(-1023.000000, -1860.000000)"
-        fill="#CED0DA"
-      >
-        <g
-          id="Dropdowns-&amp;-Selector"
-          transform="translate(187.000000, 1392.000000)"
-        >
-          <g id="Dropdowns" transform="translate(0.000000, 428.000000)">
-            <g id="Calendar" transform="translate(825.000000, 0.000000)">
-              <g id="Date-Picker" transform="translate(0.000000, 30.000000)">
-                <g id="Icon-+-Text" transform="translate(11.000000, 10.000000)">
-                  <path
-                    d="M4,9 L6,9 L6,7 L4,7 L4,9 Z M4,12 L6,12 L6,10 L4,10 L4,12 Z M7,9 L9,9 L9,7 L7,7 L7,9 Z M7,12 L9,12 L9,10 L7,10 L7,12 Z M10,9 L12,9 L12,7 L10,7 L10,9 Z M10,12 L12,12 L12,10 L10,10 L10,12 Z M14,6 C14,5.448 13.552,5 13,5 L3,5 C2.448,5 2,5.448 2,6 L2,13 C2,13.552 2.448,14 3,14 L13,14 C13.552,14 14,13.552 14,13 L14,6 Z M14,16 L2,16 C0.896,16 0,15.105 0,14 L0,2 C0,0.895 0.896,0 2,0 L4,0 L4,1 C4,1.552 4.448,2 5,2 C5.552,2 6,1.552 6,1 L6,0 L10,0 L10,1 C10,1.552 10.448,2 11,2 C11.552,2 12,1.552 12,1 L12,0 L14,0 C15.105,0 16,0.895 16,2 L16,14 C16,15.105 15.105,16 14,16 L14,16 Z"
-                    id="Calendar-Icon"
-                  />
-                </g>
-              </g>
-            </g>
-          </g>
-        </g>
-      </g>
-    </g>
+  <svg width="16" height="16" {...props}>
+    <path
+      fill="#CED0DA"
+      d="M4 9h2V7H4v2zm0 3h2v-2H4v2zm3-3h2V7H7v2zm0 3h2v-2H7v2zm3-3h2V7h-2v2zm0 3h2v-2h-2v2zm4-6a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V6zm0 10H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h2v1a1 1 0 0 0 2 0V0h4v1a1 1 0 0 0 2 0V0h2a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2z"
+    />
   </svg>
 );
 
