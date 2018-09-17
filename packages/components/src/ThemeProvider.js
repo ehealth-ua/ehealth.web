6ebcb25c60d6d0ef99a30ac85e8183d91c0d3a63
ephor <ephordnb@gmail.com>
2018-09-17 16:58:33 +0300

    fix(*): make consistent system-components and dependents

--- packages/components/src/ThemeProvider.js
+++ packages/components/src/ThemeProvider.js
@@ -1,10 +1,27 @@
 import React from "react";
-import { Provider } from "rebass/emotion";
+import * as Emotion from "emotion-theming";
+import system from "system-components/emotion";
 
 import defaultTheme from "./theme";
 
 const ThemeProvider = ({ theme, ...props }) => (
-  <Provider {...props} theme={{ ...defaultTheme, ...theme }} />
+  <Emotion.ThemeProvider theme={{ ...defaultTheme, ...theme }}>
+    <Root {...props} />
+  </Emotion.ThemeProvider>
 );
 
 export default ThemeProvider;
+
+const Root = system(
+  {
+    fontFamily: "sans"
+  },
+  "fontFamily",
+  {
+    "& *": {
+      boxSizing: "border-box"
+    }
+  },
+  "space",
+  "color"
+);
