7bb992904b563d826d4a1836e82dce6fa9ccdc92
ephor <ephordnb@gmail.com>
2018-09-18 10:15:50 +0300

    refactor(admin): refactor Tabs component

--- packages/admin/src/components/Tabs.js
+++ packages/admin/src/components/Tabs.js
@@ -1,15 +1,33 @@
-import React, { Component } from "react";
-import { Link as RouterLink } from "react-router-dom";
+import React from "react";
+import { Link, Match } from "@reach/router";
 import styled from "react-emotion/macro";
 import { ifProp } from "styled-tools";
 
-import { Match } from "@ehealth/components";
+/**
+ * @example
+ *
+ * ```jsx
+ * import Tabs from "../../components/Tabs";
+
+ * <Tabs.Nav>
+ *   <Tabs.NavItem to="./">Особиста інформація</Tabs.NavItem>
+ *   <Tabs.NavItem to="auth">Метод аутентифікації</Tabs.NavItem>
+ *   <Tabs.NavItem to="declarations">Декларації</Tabs.NavItem>
+ * </Tabs.Nav>
+ * <Tabs.Content>
+ *   <Router>
+ *     <UserInfo path="./" />
+ *     <AuthInfo path="auth" />
+ *     <DeclarationsInfo path="declarations" />
+ *   </Router>
+ * </Tabs.Content>
+*/
 
 const NavItem = ({ to, ...props }) => (
-  <Match path={to}>
-    {({ to, active }) => (
-      <TabItem active={active}>
-        <Link to={to} {...props} />
+  <Match path={`${to}`}>
+    {({ match }) => (
+      <TabItem active={match && match.path === to}>
+        <NavLink to={to} {...props} />
       </TabItem>
     )}
   </Match>
@@ -27,25 +45,31 @@ const Nav = styled.ul`
 `;
 
 const TabItem = styled.li`
-  padding: 16px 22px;
   margin-bottom: -1px;
   background-color: ${ifProp("active", "#fff")};
   border: 1px solid ${ifProp("active", "#dfe2e5", "transparent")};
-  border-width: 1px 1px 0 1px;
+  border-bottom: 0;
   white-space: nowrap;
   cursor: pointer;
 `;
 
-const Link = styled(RouterLink)`
+const NavLink = styled(Link)`
+  display: block;
+  padding: 16px 22px;
   color: #354052;
   text-decoration: none;
 `;
 
 const Content = styled.div`
-  border: 1px solid #dfe2e5;
-  border-width: 0 1px 1px 1px;
-  padding: 50px 42px;
+  padding: 10px;
+  border-width: 0 1px 0 1px;
+  border-color: #dfe2e5;
+  border-style: solid;
+  &:last-child {
+    border-width: 0 1px 1px 1px;
+  }
 `;
 
 const Tabs = { Nav, NavItem, Content };
+
 export default Tabs;
