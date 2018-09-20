289e134b16585645424bfbc4c33023d6403f80a4
ephor <ephordnb@gmail.com>
2018-09-20 14:16:16 +0300

    fix(admin): fix settlementType

--- packages/admin/src/components/AddressView.js
+++ packages/admin/src/components/AddressView.js
@@ -34,7 +34,7 @@ const AddressView = ({
     {formatArea(area)}, {region && <>{normalizeName(region)} район, </>}
     <>
       {/* TODO: Add Dictionary*/}
-      settlementType &nbsp;
+      {settlementType} &nbsp;
       {normalizeName(settlement)}
     </>
     ,{" "}
@@ -42,7 +42,7 @@ const AddressView = ({
       {street && (
         <>
           {/* TODO: Add Dictionary*/}
-          {streetType && streetType} {normalizeName(street)}
+          {streetType} {normalizeName(street)}
           &nbsp;
         </>
       )}
