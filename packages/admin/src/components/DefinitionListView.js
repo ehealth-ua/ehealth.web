5f1cf87b8872b4548be9ebd33db98663eb133399
ephor <ephordnb@gmail.com>
2018-09-21 14:16:29 +0300

    fix(admin): add default value to labelWidth

--- packages/admin/src/components/DefinitionListView.js
+++ packages/admin/src/components/DefinitionListView.js
@@ -1,25 +1,26 @@
 import React from "react";
 import { Flex, Box, Text } from "rebass/emotion";
 import system from "system-components/emotion";
+import { px } from "styled-system";
 
 import { DefinitionList } from "@ehealth/components";
 
-const DefinitionListView = ({ title, labels, data, color, labelWidth }) => (
+const DefinitionListView = ({
+  title,
+  labels,
+  data,
+  color,
+  labelWidth = 150
+}) => (
   <DefinitionList
     labels={labels}
     data={data}
     renderItem={({ label, value }) => (
       <Item color={color}>
-        <Box width={labelWidth ? labelWidth : "150px"}>
+        <Box width={labelWidth}>
           <Text fontWeight={700}>{label}</Text>
         </Box>
-        <Box
-          width={
-            labelWidth ? `calc(100% - ${labelWidth})` : "calc(100% - 150px)"
-          }
-        >
-          {value}
-        </Box>
+        <Box width={`calc(100% - ${px(labelWidth)})`}>{value}</Box>
       </Item>
     )}
   />
