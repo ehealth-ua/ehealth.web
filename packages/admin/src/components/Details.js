7d1e5da0419a106f245da05d6a6bce375c76592d
Myroslav Kiurchev <kiurchv@gmail.com>
2018-09-24 14:53:09 +0300

    feat(admin): add Details component

---
+++ packages/admin/src/components/Details.js
@@ -0,0 +1,53 @@
+import React from "react";
+import system from "system-components/emotion";
+import { mixed } from "@ehealth/system-tools";
+import { ChevronBottomIcon } from "@ehealth/icons";
+
+const Details = ({ summary, children }) => (
+  <details>
+    <Summary>
+      {summary}
+      <Icon />
+    </Summary>
+    {children}
+  </details>
+);
+
+export default Details;
+
+const Summary = system(
+  {
+    is: "summary",
+    display: "flex",
+    alignItems: "center",
+    color: "rockmanBlue",
+    fontSize: 1,
+    mb: 5
+  },
+  props => ({
+    userSelect: "none",
+    "&::-webkit-details-marker": mixed({
+      borderTop: 1,
+      borderColor: "januaryDawn",
+      display: "block",
+      flexGrow: 1,
+      order: 9999,
+      height: 0,
+      ml: 3,
+      mr: 0
+    })(props)
+  })
+);
+
+const Icon = system(
+  {
+    is: ChevronBottomIcon,
+    ml: 2,
+    width: 11
+  },
+  {
+    "details[open] &": {
+      transform: "rotate(180deg)"
+    }
+  }
+);
