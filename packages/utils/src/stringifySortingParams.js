d15461a305e5cf907c597094eca7ce10f9b31fb4
andriy-ilin <andriy.ilin90@gmail.com>
2018-10-09 14:33:33 +0300

    fix(utils): check empty params in stringifySortingParams

--- packages/utils/src/stringifySortingParams.js
+++ packages/utils/src/stringifySortingParams.js
@@ -1,6 +1,10 @@
+import isEmpty from "lodash/isEmpty";
 import snakeCase from "lodash/snakeCase";
 
-const stringifySortingParams = ({ name, order }) =>
-  [snakeCase(name), order].join("_").toUpperCase();
+const stringifySortingParams = (params = {}) => {
+  const { name, order } = params;
+  if (isEmpty(params)) return;
+  return [snakeCase(name), order].join("_").toUpperCase();
+};
 
 export default stringifySortingParams;
