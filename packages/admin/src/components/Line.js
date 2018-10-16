eb85fe6d92d2d49b591c04e5778f4c41183c9022
andriy-ilin <andriy.ilin90@gmail.com>
2018-10-16 18:43:10 +0300

    feat(admin): add Line to components

---
+++ packages/admin/src/components/Line.js
@@ -0,0 +1,13 @@
+import React from "react";
+import system from "system-components/emotion";
+
+const Line = system({
+  is: "figure",
+  bg: "januaryDawn",
+  my: 5,
+  mx: 0,
+  height: "1px",
+  w: "100%"
+});
+
+export default Line;
