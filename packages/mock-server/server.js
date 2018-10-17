edd72a205eb6f346b00616966d602889706677c0
ephor <ephordnb@gmail.com>
2018-10-17 13:55:21 +0300

    feat(mock-server): Add remote schema to mock-server

--- packages/mock-server/server.js
+++ packages/mock-server/server.js
@@ -1,11 +1,32 @@
 require("@ehealth/env/load");
-const { ApolloServer } = require("apollo-server");
+const { buildClientSchema } = require("graphql");
+const {
+  mergeSchemas,
+  makeExecutableSchema,
+  addMockFunctionsToSchema,
+  makeRemoteExecutableSchema,
+  ApolloServer
+} = require("apollo-server");
+const { setContext } = require("apollo-link-context");
+const { HttpLink } = require("apollo-link-http");
+const fetch = require("node-fetch");
 const { document: typeDefs } = require("@ehealth-ua/schema");
-
+const introspectionResult = require("./schema.json");
 const resolvers = require("./lib/resolvers");
 const schemaDirectives = require("./lib/directives");
 
 const { PORT, WHITE_LIST_ORIGINS = "" } = process.env;
+
+const http = new HttpLink({
+  uri: "https://api.dev.asclepius.com.ua/graphql",
+  fetch
+});
+
+const link = setContext((request, previousContext) => ({
+  headers: {
+    Cookie: previousContext.graphqlContext.cookie
+  }
+})).concat(http);
 
 const cors = {
   origin: WHITE_LIST_ORIGINS.split(",").map(
@@ -14,13 +35,35 @@ const cors = {
   credentials: true
 };
 
-const server = new ApolloServer({
+const introspectionResultSchema = buildClientSchema(introspectionResult.data);
+
+const mockSchema = makeExecutableSchema({
   typeDefs,
   resolvers,
-  schemaDirectives,
+  schemaDirectives
+});
+
+const remoteSchema = makeRemoteExecutableSchema({
+  schema: introspectionResultSchema,
+  link
+});
+
+addMockFunctionsToSchema({
+  schema: mockSchema
+});
+
+const schema = mergeSchemas({
+  schemas: [mockSchema, remoteSchema]
+});
+
+const server = new ApolloServer({
+  schema,
   cors,
   mocks: true,
-  mockEntireSchema: false
+  mockEntireSchema: false,
+  context: ({ req }) => ({
+    cookie: req.headers.cookie
+  })
 });
 
 server.listen(PORT, () => {
