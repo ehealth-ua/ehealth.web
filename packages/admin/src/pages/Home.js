46db33e48f98b6a470bbf1cbea3b0143cbb7f5af
Andriy Ilin <andriy-ilin@users.noreply.github.com>
2018-11-21 13:03:41 +0200

    feat(admin): add text to Home page and delete redirect to LE (#519)

--- packages/admin/src/pages/Home.js
+++ packages/admin/src/pages/Home.js
@@ -1,6 +1,28 @@
 import React from "react";
-import { Redirect } from "@reach/router";
+import { Query } from "react-apollo";
+import { Box, Heading, Text } from "rebass/emotion";
+import { ITEMS_PER_PAGE } from "../constants/pagination";
+import SearchLegalEntitiesQuery from "../graphql/SearchLegalEntitiesQuery.graphql";
 
-const Home = () => <Redirect to="legal-entities/search" noThrow />;
+const Home = () => (
+  <Query
+    query={SearchLegalEntitiesQuery}
+    variables={{
+      first: ITEMS_PER_PAGE[0]
+    }}
+  >
+    {({ loading, error, data }) => {
+      if (loading || error) return null;
+      return (
+        <Box p={6}>
+          <Heading as="h1" fontWeight="normal" mb={4}>
+            Вітаємо в адміністративній системі Ehealth!
+          </Heading>
+          <Text>Оберіть пункт меню ліворуч, щоб почати роботу!</Text>
+        </Box>
+      );
+    }}
+  </Query>
+);
 
 export default Home;
