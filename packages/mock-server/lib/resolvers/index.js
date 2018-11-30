702f8e19ca32bc91b5f0c933a66b73ee35a21dd8
andriy-ilin <andriy.ilin90@gmail.com>
2018-11-30 18:29:25 +0200

    feat(mock-server): add new resolvers for scalars

--- packages/mock-server/lib/resolvers/index.js
+++ packages/mock-server/lib/resolvers/index.js
@@ -1,10 +1,17 @@
-const { GraphQLDate, GraphQLDateTime } = require("graphql-iso-date");
+const {
+  GraphQLDate,
+  GraphQLDateTime,
+  GraphQLTime
+} = require("graphql-iso-date");
 const GraphQLJSON = require("graphql-type-json");
+const GraphQLUUID = require("graphql-tools-type-uuid");
 
 const resolvers = {
   Date: GraphQLDate,
   DateTime: GraphQLDateTime,
-  JSON: GraphQLJSON
+  Time: GraphQLTime,
+  JSON: GraphQLJSON,
+  UUID: GraphQLUUID
 };
 
 module.exports = resolvers;
