ef72e270cc37f4158846fc792078a0ae998b821a
andriy-ilin <andriy.ilin90@gmail.com>
2019-01-18 17:51:43 +0200

    fix(admin-legacy): add to white list empty string for checking type in file on RegisterUploadForm

--- packages/admin-legacy/src/helpers/validators/isFileTypeCsv.js
+++ packages/admin-legacy/src/helpers/validators/isFileTypeCsv.js
@@ -7,7 +7,11 @@ const WHITE_LIST = [
   "text/csv",
   "text/comma-separated-values",
   "text/x-comma-separated-values",
-  "text/tab-separated-values"
+  "text/tab-separated-values",
+  // Uncommon file extensions would return an empty string.
+  // Client configuration (for instance, the Windows Registry)
+  // may result in unexpected values even for common types.
+  ""
 ];
 
 const isFileTypeCsv = value => value && WHITE_LIST.includes(value.type);
