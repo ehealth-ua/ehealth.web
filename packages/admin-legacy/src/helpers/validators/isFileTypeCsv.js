cbeb77c24385c2d98b59d15b3feb3089b7563e52
andriy-ilin <andriy.ilin90@gmail.com>
2019-01-18 14:16:41 +0200

    fix(admin-legacy): checking csv type with list in validation on RegisterUploadForm

--- packages/admin-legacy/src/helpers/validators/isFileTypeCsv.js
+++ packages/admin-legacy/src/helpers/validators/isFileTypeCsv.js
@@ -1,4 +1,15 @@
-const isFileTypeCsv = value =>
-  value && value.type !== "text/csv" ? false : true;
+const WHITE_LIST = [
+  "text/plain",
+  "text/x-csv",
+  "application/vnd.ms-excel",
+  "text/x-csv",
+  "application/x-csv",
+  "text/csv",
+  "text/comma-separated-values",
+  "text/x-comma-separated-values",
+  "text/tab-separated-values"
+];
+
+const isFileTypeCsv = value => value && WHITE_LIST.includes(value.type);
 
 export default isFileTypeCsv;
