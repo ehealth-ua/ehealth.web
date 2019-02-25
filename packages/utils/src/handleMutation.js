f99e2e290c66fe82b28a2a775bba8d06f6eb6b5e
ephor <ephordnb@gmail.com>
2019-02-25 14:10:59 +0200

    refactor(utils): remove handleMutation

--- packages/utils/src/handleMutation.js
+++
@@ -1,9 +0,0 @@
-import { normalizeErrors } from "./";
-
-const handleMutation = async (mutation, prefix) => {
-  const { errors, data } = await mutation;
-  if (errors) throw normalizeErrors(errors, prefix);
-  return data;
-};
-
-export default handleMutation;
