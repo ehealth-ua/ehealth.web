87721c78ca18bf74260f7b12042013e13fc7f54f
ephor <ephordnb@gmail.com>
2019-03-01 09:41:16 +0200

    feat(admin): remove the required number of characters from the regular expression

--- packages/admin/src/constants/contractRequests.js
+++ packages/admin/src/constants/contractRequests.js
@@ -1,4 +1,4 @@
-export const EDRPOU_PATTERN = "^[0-9]{8,10}$";
+export const EDRPOU_PATTERN = "^[0-9]";
 export const CONTRACT_REQUEST_PATTERN =
   "^[0-9A-Za-zА-ЯҐЇІЄа-яґїіє]{4}-[0-9A-Za-zА-ЯҐЇІЄа-яґїіє]{4}-[0-9A-Za-zА-ЯҐЇІЄа-яґїіє]{4}$";
 export const SEARCH_REQUEST_PATTERN = `(${EDRPOU_PATTERN})|(${CONTRACT_REQUEST_PATTERN})`;
