50e28920edd0457a0b9799ee877804a9abe18cb5
ephor <ephordnb@gmail.com>
2019-03-06 13:44:44 +0200

    refactor(admin): remove unused env variable, and fetchLabels func

--- packages/admin-legacy/src/redux/labels.js
+++ packages/admin-legacy/src/redux/labels.js
@@ -1,21 +1,4 @@
 import { handleAction, combineActions } from "redux-actions";
-import env from "../env";
-import { createUrl } from "../helpers/url";
-import { invoke } from "./api";
-
-export const fetchLabels = options =>
-  invoke({
-    endpoint: createUrl(`${env.REACT_APP_MOCK_API_URL}/labels`, options),
-    method: "GET",
-    headers: {
-      "content-type": "application/json"
-    },
-    types: [
-      "labels/FETCH_LIST_REQUEST",
-      "labels/FETCH_LIST_SUCCESS",
-      "labels/FETCH_LIST_FAILURE"
-    ]
-  });
 
 export default handleAction(
   combineActions("labels/FETCH_LIST_SUCCESS", "templates/FETCH_LIST_SUCCESS"),
