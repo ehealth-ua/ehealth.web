8d393f09d34909da4785e6299816180ab0d3e998
burlachenkovv <val.volkov@gmail.com>
2019-05-28 10:11:44 +0300

    feat: add Create Service popup and mutation

---
+++ packages/admin/src/pages/Services/Search/CreateServicePopup.js
@@ -0,0 +1,125 @@
+import React, { useState } from "react";
+import gql from "graphql-tag";
+import { Trans } from "@lingui/macro";
+import Composer from "react-composer";
+import { Mutation } from "react-apollo";
+import { Flex, Box } from "@rebass/emotion";
+import { Form, Validation } from "@ehealth/components";
+
+import Popup from "../../../components/Popup";
+import Button from "../../../components/Button";
+import * as Field from "../../../components/Field";
+import DictionaryValue from "../../../components/DictionaryValue";
+
+const CreateServicePopup = ({ locationParams, refetchQuery }) => {
+  const [isPopupVisible, setPopupVisibility] = useState(false);
+  const toggle = () => setPopupVisibility(!isPopupVisible);
+
+  return (
+    <Box>
+      <Mutation
+        mutation={CreateServiceMutation}
+        refetchQueries={() => [
+          {
+            query: refetchQuery,
+            variables: locationParams
+          }
+        ]}
+      >
+        {createService => (
+          <>
+            <Button onClick={toggle} variant="green">
+              <Trans>Create service</Trans>
+            </Button>
+            <Popup
+              visible={isPopupVisible}
+              onCancel={toggle}
+              title={<Trans>Create service</Trans>}
+              okButtonProps={{ variant: "green" }}
+              justifyButtons="left"
+              formId="createService"
+            >
+              <Form
+                id="createService"
+                onSubmit={async input => {
+                  await createService({
+                    variables: { input }
+                  });
+                  toggle();
+                }}
+              >
+                <Trans
+                  id="Enter service name"
+                  render={({ translation }) => (
+                    <Field.Text
+                      name="name"
+                      label={<Trans>Service name</Trans>}
+                      placeholder={translation}
+                      maxlength={100}
+                      showLengthHint
+                    />
+                  )}
+                />
+                <Validation.Required field="name" message="Required field" />
+                <Flex mx={-1}>
+                  <Box px={1} width={1 / 2}>
+                    <Trans
+                      id="Enter service code"
+                      render={({ translation }) => (
+                        <Field.Text
+                          name="code"
+                          label={<Trans>Service code</Trans>}
+                          placeholder={translation}
+                          maxlength={10}
+                          showLengthHint
+                        />
+                      )}
+                    />
+                    <Validation.Required
+                      field="code"
+                      message="Required field"
+                    />
+                  </Box>
+                  <Box px={1} width={1 / 2}>
+                    <Composer
+                      components={[
+                        <DictionaryValue name="SERVICE_CATEGORY" />,
+                        ({ render }) => (
+                          <Trans id="Select option" render={render} />
+                        )
+                      ]}
+                    >
+                      {([dict, { translation }]) => (
+                        <Field.Select
+                          name="category"
+                          label={<Trans>Service category</Trans>}
+                          placeholder={translation}
+                          items={Object.keys(dict)}
+                          itemToString={item => dict[item] || translation}
+                          variant="select"
+                          emptyOption
+                        />
+                      )}
+                    </Composer>
+                  </Box>
+                </Flex>
+              </Form>
+            </Popup>
+          </>
+        )}
+      </Mutation>
+    </Box>
+  );
+};
+
+const CreateServiceMutation = gql`
+  mutation CreateServiceMutation($input: CreateServiceInput!) {
+    createService(input: $input) {
+      service {
+        id
+      }
+    }
+  }
+`;
+
+export default CreateServicePopup;
--- packages/admin/src/pages/Services/Search/index.js
+++ packages/admin/src/pages/Services/Search/index.js
@@ -6,71 +6,67 @@ import { Trans } from "@lingui/macro";
 import { Flex, Box, Heading } from "@rebass/emotion";
 import { LocationParams } from "@ehealth/components";
 
+import Ability from "../../../components/Ability";
 import Pagination from "../../../components/Pagination";
 import SearchForm from "../../../components/SearchForm";
 import LoadingOverlay from "../../../components/LoadingOverlay";
 import filteredLocationParams from "../../../helpers/filteredLocationParams";
 
-import { PrimarySearchFields } from "./SearchFields";
 import ServicesTable from "./ServicesTable";
+import { PrimarySearchFields } from "./SearchFields";
+import CreateServicePopup from "./CreateServicePopup";
 
-const Search = () => {
-  //TODO: use state for handling `create` popup visibility
-  return (
-    <Box p={6}>
-      <LocationParams>
-        {({ locationParams, setLocationParams }) => {
-          return (
-            <>
-              <Flex
-                justifyContent="space-between"
-                alignItems="flex-start"
-                mb={6}
-              >
-                <Box>
-                  <Heading as="h1" fontWeight="normal" mb={4}>
-                    <Trans>Services</Trans>
-                  </Heading>
-                </Box>
-                {
-                  //TODO: Add `Create` button with Ability checking
-                }
-              </Flex>
-              <SearchForm
-                initialValues={locationParams}
-                onSubmit={setLocationParams}
-                renderPrimary={PrimarySearchFields}
+const Search = () => (
+  <Box p={6}>
+    <LocationParams>
+      {({ locationParams, setLocationParams }) => (
+        <>
+          <Flex justifyContent="space-between" alignItems="flex-start" mb={6}>
+            <Box>
+              <Heading as="h1" fontWeight="normal" mb={4}>
+                <Trans>Services</Trans>
+              </Heading>
+            </Box>
+            <Ability action="write" resource="service_catalog">
+              <CreateServicePopup
+                locationParams={filteredLocationParams(locationParams)}
+                refetchQuery={SearchServicesQuery}
               />
-              <Query
-                query={SearchServicesQuery}
-                fetchPolicy="network-only"
-                variables={filteredLocationParams(locationParams)}
-              >
-                {({
-                  loading,
-                  error,
-                  data: { services: { nodes: services = [], pageInfo } = {} }
-                }) => {
-                  if (isEmpty(services)) return null;
-                  return (
-                    <LoadingOverlay loading={loading}>
-                      <ServicesTable
-                        locationParams={locationParams}
-                        setLocationParams={setLocationParams}
-                        services={services}
-                      />
-                      <Pagination {...pageInfo} />
-                    </LoadingOverlay>
-                  );
-                }}
-              </Query>
-            </>
-          );
-        }}
-      </LocationParams>
-    </Box>
-  );
-};
+            </Ability>
+          </Flex>
+          <SearchForm
+            initialValues={locationParams}
+            onSubmit={setLocationParams}
+            renderPrimary={PrimarySearchFields}
+          />
+          <Query
+            query={SearchServicesQuery}
+            fetchPolicy="network-only"
+            variables={filteredLocationParams(locationParams)}
+          >
+            {({
+              loading,
+              error,
+              data: { services: { nodes: services = [], pageInfo } = {} }
+            }) => {
+              if (isEmpty(services)) return null;
+              return (
+                <LoadingOverlay loading={loading}>
+                  <ServicesTable
+                    locationParams={locationParams}
+                    setLocationParams={setLocationParams}
+                    services={services}
+                  />
+                  <Pagination {...pageInfo} />
+                </LoadingOverlay>
+              );
+            }}
+          </Query>
+        </>
+      )}
+    </LocationParams>
+  </Box>
+);
 
 const SearchServicesQuery = gql`
   query SearchServicesQuery(
