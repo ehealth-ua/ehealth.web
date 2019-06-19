92a44d9f036a48a6d312163590e52c1be61815ef
burlachenkovv <val.volkov@gmail.com>
2019-06-19 09:40:04 +0300

    feat(admin): add variants to the InfoBox component

--- packages/admin/src/components/InfoBox.js
+++ packages/admin/src/components/InfoBox.js
@@ -4,15 +4,26 @@ import system from "@ehealth/system-components";
 const InfoBox = system(
   {
     extend: Box,
-    p: 4,
-    my: 5,
+    variant: "default",
     fontSize: 0,
     border: 1,
     borderColor: "januaryDawn"
   },
   {
     position: "relative",
-    lineHeight: 1.5,
+    lineHeight: 1.5
+  },
+  ({ variant }) => boxes[variant],
+  "space",
+  "fontSize",
+  "border",
+  "borderColor"
+);
+
+const boxes = {
+  default: {
+    margin: "25px 0",
+    padding: "20px",
     "&::before": {
       content: '""',
       position: "absolute",
@@ -27,10 +38,23 @@ const InfoBox = system(
       backgroundColor: "#fff"
     }
   },
-  "space",
-  "fontSize",
-  "border",
-  "borderColor"
-);
+  horizontal: {
+    margin: "0 20px",
+    padding: "8px 10px",
+    "&::before": {
+      content: '""',
+      position: "absolute",
+      display: "block",
+      top: "10px",
+      left: "-6px",
+      width: "12px",
+      height: "12px",
+      border: "1px solid #dfe2e5",
+      transform: "rotate(-45deg)",
+      clipPath: "polygon(100% 0,0 100%,0 0)",
+      backgroundColor: "#fff"
+    }
+  }
+};
 
 export default InfoBox;
